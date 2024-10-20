1760e2e869dd3638d07d68190f20719470ea55c7
leonow32 <leonow32@gmail.com>
2024-10-20 19:29:47 +0200

    Demo prostych operacji na plikach

--- 04 System plików, karta SD i EEPROM inaczej/read_write_demo.py
+++ 04 System plików, karta SD i EEPROM inaczej/read_write_demo.py
@@ -1,5 +1,24 @@
 
+def read_file_content():
+    with open("plik.txt") as file:
+        content = file.read()       # string
+    print(content)
 
-def read_file():
-    with open("app/config.json") as file:
+def read_file_lines():
+    with open("plik.txt") as file:
+        global lines
+        lines = file.readlines()    # list of strings
+    
+    for line in lines:
+        print(line)
         
+def save_file():
+    with open("plik.txt", "a") as file:
+        file.write("Pierwsza linia\n")
+        file.write("Druga linia\n")
+        file.write("Trzecia linia\n")
+        
+if __name__ == "__main__":
+    save_file()
+    read_file_lines()
+    #read_file_content()
