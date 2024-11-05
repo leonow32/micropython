495a0f66d88d86538bd798f38caf0861fe37444b
leonow32 <leonow32@gmail.com>
2024-11-05 15:50:37 +0100

    Demo wielowątkowości z asyncio

---
+++ async.py
@@ -0,0 +1,20 @@
+import asyncio
+from machine import Pin
+
+async def led_task(gpio_num, period_ms):
+    led = Pin(gpio_num, Pin.OUT)
+    
+    while True:
+        led(not led())
+        await asyncio.sleep_ms(period_ms)
+
+async def main():
+    asyncio.create_task(led_task(21, 1000))
+    asyncio.create_task(led_task(47, 500))
+    asyncio.create_task(led_task(48, 250))
+    asyncio.create_task(led_task(45, 100))
+    while True:
+        print("main")
+        await asyncio.sleep_ms(1000)
+    
+asyncio.run(main())
---
+++ threads2.py
@@ -0,0 +1,18 @@
+import _thread
+import time
+import mem_used
+from machine import Pin
+
+def test_task(text):   
+    while True:  
+        lock.acquire()
+        print(f"{time.ticks_ms()} - {text}")
+        lock.release()
+        #time.sleep_ms(1000)
+        
+
+lock = _thread.allocate_lock()
+_thread.start_new_thread(test_task, ["."])
+_thread.start_new_thread(test_task, ["X"])
+
+#mem_used.print_ram_used()
