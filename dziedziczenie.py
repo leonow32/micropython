5ea4a045d2036830615f020c258507f78ec2c5e4
leonow32 <leonow32@gmail.com>
2024-11-09 13:31:55 +0100

    Demo dziedzieczenia

---
+++ dziedziczenie.py
@@ -0,0 +1,44 @@
+class Zwierze():
+    def __init__(self, imie, wiek):
+        print(f"Zwierze.__init__({self}, {imie}, {wiek})")
+        self.imie = imie
+        self.wiek = wiek
+        
+    def przedstaw_sie(self):
+        print(f"Zwierze.przedstaw_sie({self})")
+        print(f"- Nazywam sie {self.imie}")
+        
+    def ile_masz_lat(self):
+        print(f"Zwierze.ile_masz_lat({self})")
+        print(f"- Mam {self.wiek} lat")
+ 
+class Pies(Zwierze):
+    def __init__(self, rasa, kolor):
+        print(f"Pies.__init__({self}, {rasa}, {kolor})")
+        self.rasa  = rasa
+        self.kolor = kolor
+        super().__init__("Reksio", 100)
+        
+    def szczekaj(self):
+        print(f"Pies.szczekaj({self})")
+        print(f"- Hau hau")
+        
+    def opisz_sie(self):
+        print(f"Pies.opisz_sie({self})")
+        print(f"- imie: {self.imie}")
+        print(f"- kolor: {self.kolor}")
+        print(f"- rasa: {self.rasa}")
+        print(f"- wiek: {self.wiek}")
+
+if __name__ == "__main__":
+    print("=== 1 ===")
+    zwierzak = Zwierze("Test", 10)
+    zwierzak.przedstaw_sie()
+    zwierzak.ile_masz_lat()
+    
+    print("=== 2 ===")
+    reksio = Pies("jamnik", "brazowy")
+    reksio.ile_masz_lat()
+    reksio.opisz_sie()
+    
+    
