9e2790d1ccdacc27fb8a153cfa4ce66f7d95118f
leonow32 <leonow32@gmail.com>
2024-11-17 21:23:18 +0100

    Ściąganie plików z URL

--- 05_Dostep_do_WIFI/download_file.py
+++ 05_Dostep_do_WIFI/download_file.py
@@ -19,18 +19,24 @@ def wifi_connect():
     
     print(f"Adres IP: {station.ifconfig()[0]}")
     
-def download_file():
-    
-    result = requests.get(url="https://retro.hackaday.com")
-    result = result.content.decode(encoding="utf-8")
-    #if result.code 
-    #print(result.content)
-    return result
+def download_file(url):
+    print(f"dowanload_file({url})")
+    result = requests.get(url)
 
+    if result.status_code == 200:
+        a = result.text
+        c = a.splitlines()
+        
+        for line in c:
+            print(line)
+    else:
+        print(f"Error {result.status_code}")
         
 if __name__ == "__main__":    
 
     wifi_connect()
-    result = download_file()
+    #result = download_file("https://retro.hackaday.com/about.html")
+    #result = download_file("https://ep.com.pl/robots.txt")
+    result = download_file("https://raw.githubusercontent.com/leonow32/micropython_kurs/refs/heads/main/09_Kolorowy_wyswietlacz_TFT_z_panelem_dotykowym/ft6336.py")
    
 
