98e631b1e5513c83d4cb9f65acf69fd8b8c973ae
leonow32 <leonow32@gmail.com>
2024-11-21 22:01:26 +0100

    WIP serwer HTTP

--- 06_Serwer_HTTP/http_server.py
+++ 06_Serwer_HTTP/http_server.py
@@ -1,5 +1,7 @@
 import _thread
+import esp32
 import gc
+import neopixel
 import network
 import socket
 import sys
@@ -9,6 +11,10 @@ from machine import Pin
 ssid = "Extronic2.4"
 password = "LeonInstruments"
 
+led = neopixel.NeoPixel(Pin(38, Pin.OUT), 1)
+led[0] = (0, 0, 0)
+led.write()
+
 def led_task(gpio_num, delay_ms):
     led = Pin(gpio_num, Pin.OUT)
     
@@ -17,6 +23,7 @@ def led_task(gpio_num, delay_ms):
         time.sleep_ms(delay_ms)
 
 def wifi_connect():
+    global station
     station = network.WLAN(network.STA_IF)
     station.active(True)
     if not station.isconnected():
@@ -32,7 +39,6 @@ def wifi_connect():
 def style_css():
     print("-- style.css")
     gc.collect()
-    
     content = ""
     with open("style.css") as file:
         content = file.read()
@@ -43,75 +49,47 @@ def index_html():
     print("-- index.html")
     gc.collect()
     content = ""
+    #content = "HTTP/1.1 200 OK\r\nContent-Type: text/HTML\r\nConnection: close\r\n\r\n"
+    with open("index.html", encoding="utf-8") as file:
+        content += file.read()  
     
-    with open("index.html") as file:
-        content = file.read()
-        
+    content = content.replace("AA", str(esp32.mcu_temperature()))
+    content = content.replace("BBB", str(station.status("rssi")))
     
-        
+    if led[0] == (0x40, 0x00, 0x00):
+        color = "Czerwony"
+    elif led[0] == (0x40, 0x40, 0x00):
+        color = "Żółty"
+    elif led[0] == (0x00, 0x40, 0x00):
+        color = "Zielony"
+    elif led[0] == (0x00, 0x40, 0x40):
+        color = "Błękitny"
+    elif led[0] == (0x00, 0x00, 0x40):
+        color = "Niebieski"
+    elif led[0] == (0x40, 0x00, 0x40):
+        color = "Fioletowy"
+    elif led[0] == (0x40, 0x40, 0x40):
+        color = "Biały"
+    elif led[0] == (0x10, 0x10, 0x10):
+        color = "Szary"
+    else:
+        color = "Czarny"
     
-    return content
-
-def test_txt():
-    print("-- test.txt")
-    gc.collect()
-    content = "To jest plik test.txt"
-    return content
-
-def time_txt():
-    print("-- time.txt")
-    content = f"""
-HTTP/1.1 200 OK
-Content-Type: text/plain
-Connection: close
-
-Teraz jest czas
-{str(time.ticks_ms())}
-ms
-        
-    """
-    #content = str(time.ticks_ms())
+    content = content.replace("CCCCCCCCC", color)
+    
     return content
 
 def time_html():
     print("-- time.html")
-    content = f"""
-HTTP/1.1 200 OK
-Content-Type: text/html
-Connection: close
+    content  = "HTTP/1.1 200 OK\r\n"
+    #content  = "HTTP/1.1 404 Not Found"
+    content += "Content-Type: text/html\r\n"
+    content += "Connection: close\r\n\r\n"
 
-Teraz jest czas
-{str(time.ticks_ms())}
-ms
-
-    """
-    #content = str(time.ticks_ms())
+    content += "Teraz jest czas\r\n"
+    content += f"{str(time.ticks_ms())} ms"
+    
     return content
-
-# czy to potrzebne?
-def parse_params(data):
-    print("parse_params()")
-    parameters = {}
-    
-    # Remove everything before '?'
-    temp = data.find(b'?') + 1
-    data = data[temp:]
-    
-    # Remove everything after ' '
-    temp = data.find(b' ')
-    data = data[:temp]
-    
-    # Split lines
-    data = data.split(b'&')
-    
-    for item in data:
-        temp = item.split(b'=')
-        temp[0] = temp[0].decode()
-        temp[1] = temp[1].decode()
-        parameters[temp[0]] = temp[1]
-        print(f"{temp[0]} = {temp[1]}")
-    
-    return parameters
 
 def task():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -139,8 +117,14 @@ def task():
             request = conn.recv(1024)
             #conn.settimeout(None)
             
+            if request == b"":
+                print("Empty request")
+                continue
+            
             # Save only the first line of the request
-            print(f"-- HTTP Request from {addr[0]}: {request[0:60]}")
+            #if b"\n" in request:
+            request = request.splitlines()[0]
+            print(f"-- HTTP Request from {addr[0]}: {request}")
             #print(request)
             
             # Prepare response
@@ -148,24 +132,66 @@ def task():
             
             if (b"GET / HTTP" in request):
                 response = index_html()
+                conn.send("HTTP/1.1 200 OK\n")
+                conn.send("Content-Type: text/html\n")
+                conn.send("Connection: close\n\n")
+                conn.sendall(response)
                 
-            elif b"style.css" in request:
+            elif b"style.css " in request:
                 response = style_css()
+                conn.send("HTTP/1.1 200 OK\n")
+                conn.send("Content-Type: text/css\n")
+                conn.send("Connection: close\n\n")
+                conn.sendall(response)
             
-            elif b"index.html" in request:
+            elif b"index.html " in request:
                 response = index_html()
-            
-            elif b"test.txt" in request:
-                response = test_txt()
-                
-            elif b"time.txt" in request:
-                response = time_txt()
+                conn.send("HTTP/1.1 200 OK\n")
+                conn.send("Content-Type: text/html\n")
+                conn.send("Connection: close\n\n")
+                conn.sendall(response)
                 
             elif b"time.html" in request:
                 response = time_html()
-            
-            #elif b"favicon.ico" in request:
-            #    response = "favicon"
+                
+            elif b"color" in request:
+                if b"red" in request:
+                    print(f"red {request}")
+                    led[0] = (0x40, 0, 0)
+                elif b"yellow" in request:
+                    print(f"yellow {request}")
+                    led[0] = (0x40, 0x40, 0)
+                elif b"green" in request:
+                    print(f"green {request}")
+                    led[0] = (0, 0x40, 0)
+                elif b"cyan" in request:
+                    print(f"cyan {request}")
+                    led[0] = (0, 0x40, 0x40)
+                elif b"blue" in request:
+                    print(f"blue {request}")
+                    led[0] = (0, 0x0, 0x40)
+                elif b"magenta" in request:
+                    print(f"magenta {request}")
+                    led[0] = (0x40, 0x00, 0x40)
+                elif b"white" in request:
+                    print(f"white {request}")
+                    led[0] = (0x40, 0x40, 0x40)
+                elif b"grey" in request:
+                    print(f"gray {request}")
+                    led[0] = (0x10, 0x10, 0x10)
+                else:
+                    print(f"black {request}")
+                    led[0] = (0x00, 0x00, 0x00)
+                    
+                led.write()
+                
+                
+                response = index_html()
+                
+                conn.send("HTTP/1.1 200 OK\n")
+                conn.send("Content-Type: text/html\n")
+                conn.send("Connection: close\n\n")
+                conn.sendall(response)
                 
             else:
                 print("Unknown request")
@@ -173,12 +199,10 @@ def task():
                 #global local_ip
                 #response = f"HTTP/1.1 307 Temporary Redirect\r\nLocation: http://{local_ip}/index.html\r\n\r\n"
             
-            #conn.send("HTTP/1.1 200 OK\n")
-            #conn.send("Content-Type: text/plain\n")
-            #conn.send("Connection: close\n\n")
-            conn.send(response)
+            
+            #conn.sendall(response)
             conn.close()
-                       
+            
         except Exception as e:
             print(f"Exception {e}")
             sys.print_exception(e)
@@ -186,9 +210,6 @@ def task():
             time.sleep_ms(100)
 
 def init():
-    pass
-
-def run_task():
     _thread.start_new_thread(task, ())
 
 if __name__ == "__main__":
@@ -197,6 +218,5 @@ if __name__ == "__main__":
     _thread.start_new_thread(led_task, [48, 250])
     _thread.start_new_thread(led_task, [45, 100])
     wifi_connect()
-    #simple()
-    run_task()
+    init()
 
