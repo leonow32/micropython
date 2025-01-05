21ba1d282ee2b02946f47e4b32abeffcfc2b511f
leonow32 <leonow32@gmail.com>
2025-01-05 21:38:09 +0100

    Czcionka DOS16

---
+++ Font/font_dos16_demo.py
@@ -0,0 +1,39 @@
+from machine import Pin, I2C
+from font.dos16 import *
+import framebuf
+import ssd1309
+import simulator
+import mem_used
+import time       
+
+button = Pin(0, Pin.IN, Pin.PULL_UP)
+i2c = I2C(0, scl=Pin(1), sda=Pin(2), freq=400000)
+display = ssd1309.SSD1309(i2c)
+#display = simulator.SIM()
+
+char = 0
+for page in range(4):
+    for row in range(4):
+        for col in range(16):
+            string = chr(char)
+            display.print_text(dos16, string, col*8, row*16)
+            char += 1
+    display.refresh()
+    time.sleep_ms(100)
+    while button(): pass
+
+char = 0
+for page in range(4):
+    for row in range(4):
+        for col in range(16):
+            string = chr(char)
+            display.print_text(dos16, string, col*8, row*16, 0, 0)
+            char += 1
+    display.refresh()
+    time.sleep_ms(100)
+    while button(): pass
+
+mem_used.print_ram_used()
+
+
+
