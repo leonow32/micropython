bfffe7a9d6c336093fd461383bfd37c50341c4c2
leonow32 <leonow32@gmail.com>
2025-01-20 20:30:31 +0100

    Uproszczenie kodu

--- 02_Timery_przerwania_dioda_WS2812/interrupt_demo.py
+++ 02_Timery_przerwania_dioda_WS2812/interrupt_demo.py
@@ -6,30 +6,26 @@ import random
 from machine import Pin, Timer
 
 def timer_int(source):
-    print(f"Przerwanie od {source} --")
+    print(f"Przerwanie od {source}")
     
-    global led
     led[0] = (0, 0, 0)
     led.write()
 
 def button_int(source):
-    print(f"Przerwanie od {source} --")
+    print(f"Przerwanie od {source}")
     
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
     
-    global led
     led[0] = (r, g, b)
     led.write()
     
-    print(f"- Wylosowany kolor to: {led[0]}")
+    print(f"Wylosowany kolor to: {led[0]}")
     
     Timer(0, mode=Timer.ONE_SHOT, period=2000, callback=timer_int)
 
-if __name__ == "__main__":
-    global led
-    led = neopixel.NeoPixel(Pin(38, Pin.OUT), 1)
-    button = Pin(0, Pin.IN, Pin.PULL_UP)
-    button.irq(trigger=Pin.IRQ_FALLING, handler=button_int)
-    mem_used.print_ram_used()
+led = neopixel.NeoPixel(Pin(38, Pin.OUT), 1)
+button = Pin(0, Pin.IN, Pin.PULL_UP)
+button.irq(trigger=Pin.IRQ_FALLING, handler=button_int)
+mem_used.print_ram_used()
