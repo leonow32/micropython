71389d4f3b328c8548dff8b1e510ee24b58157e9
leonow32 <leonow32@gmail.com>
2025-01-20 21:20:59 +0100

    Uproszczenie kodu

--- 02_Timery_przerwania_dioda_WS2812/interrupt_demo.py
+++ 02_Timery_przerwania_dioda_WS2812/interrupt_demo.py
@@ -25,7 +25,7 @@ def button_int(source):
     
     Timer(0, mode=Timer.ONE_SHOT, period=2000, callback=timer_int)
 
-led = neopixel.NeoPixel(Pin(38, Pin.OUT), 1)
 button = Pin(0, Pin.IN, Pin.PULL_UP)
 button.irq(trigger=Pin.IRQ_FALLING, handler=button_int)
+led = neopixel.NeoPixel(Pin(38, Pin.OUT), 1)
 mem_used.print_ram_used()
