676d2f5032d07e12e2542481d49c17026570bdb2
leonow32 <leonow32@gmail.com>
2025-01-31 14:05:16 +0100

    Nowe konwerter obrazków

--- 05_Wyswietlacz_OLED/image_demo.py
+++ 05_Wyswietlacz_OLED/image_demo.py
@@ -1,30 +1,46 @@
 # MicroPython 1.24.1 ESP32-S3 Octal SPIRAM
 
 from machine import Pin, I2C
-import images
 import ssd1309
 import mem_used
 import time
+
+from image.back_32x32 import *
+from image.book_32x32 import *
+from image.cancel_32x32 import *
+from image.clock_32x32 import *
+from image.down_32x32 import *
+from image.ep_logo_128x40 import *
+from image.hand_32x32 import *
+from image.light_32x32 import *
+from image.ok_32x32 import *
+from image.settings_32x32 import *
+from image.square_8x16 import *
+from image.test_16x16 import *
+from image.up_32x32 import *
+from image.world_128x64 import *
 
 i2c = I2C(0, scl=Pin(1), sda=Pin(2), freq=400000)
 display = ssd1309.SSD1309(i2c)
 
 start_time = time.ticks_us()
-display.blit(images.ok_32x32,        0,  0)
-display.blit(images.back_32x32,      0, 32)
-display.blit(images.clock_32x32,    32,  0)
-display.blit(images.settings_32x32, 32, 32)
-display.blit(images.book_32x32,     64,  0)
-display.blit(images.light_32x32,    64, 32)
-display.blit(images.up_32x32,       96,  0)
-display.blit(images.down_32x32,     96, 32)
+display.blit(ok_32x32,        0,  0)
+display.blit(back_32x32,      0, 32)
+display.blit(clock_32x32,    32,  0)
+display.blit(settings_32x32, 32, 32)
+display.blit(book_32x32,     64,  0)
+display.blit(light_32x32,    64, 32)
+display.blit(up_32x32,       96,  0)
+display.blit(down_32x32,     96, 32)
 
-# display.blit(images.world_128x64, 0, 0)
-# display.blit(images.world_128x64, 0, 0, 0)  # tło przezroczyste
+# display.blit(world_128x64, 0, 0)
+# display.blit(world_128x64, 0, 0, 0)  # tło przezroczyste
 
-# display.blit(images.ep_logo_128x40, 0, 0)
+# display.blit(ep_logo_128x40, 0, 0)
 
 end_time = time.ticks_us()
 display.refresh()
 
 mem_used.print_ram_used()
+
+
