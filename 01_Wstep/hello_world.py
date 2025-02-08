abc19d9a62c4924a6d48fada68d33e92c253271c
leonow32 <leonow32@gmail.com>
2025-02-08 16:58:18 +0100

    Odcinek 1 gotowy

--- 01_Wstep/hello_world.py
+++ 01_Wstep/hello_world.py
@@ -1,12 +1,13 @@
+# MicroPython 1.24.1 ESP32-S3 Octal SPIRAM
+
 import time
 from machine import Pin
 
 print("Hello world!")
 
 button = Pin(0, Pin.IN, Pin.PULL_UP)
-led = Pin(21, Pin.OUT)
-
-name = input("Jak masz na imię? ")
+led    = Pin(21, Pin.OUT)
+name   = input("Jak masz na imię? ")
 print(f"Cześć {name}!!!")
 
 print("Naciśnij przycisk")
