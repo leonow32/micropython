ce35739afad4c594a4ffa1670d7695cb336369a0
leonow32 <leonow32@gmail.com>
2025-03-12 21:01:52 +0100

    Improved I2C scanner

--- utils/i2c-scan.py
+++ utils/i2c-scan.py
@@ -7,30 +7,34 @@ i2c = I2C(0)
 devices = i2c.scan()
 
 library = {
-    0x18: "LIS331   Accelerometer / MCP9808 Temperature Sensor",
-    0x20: "XL9535   GPIO Port",
-    0x21: "SDP31    Air Pressure Sensor",
-    0x32: "ZMOD4410 TVOC Sensor",
-    0x38: "AHT20    Temperature & Humidity Sensor / FT6336 Touch Panel",
-    0x39: "APDS9960 Gesture Sensor / VCNL4020 Light Sensor",
-    0x3C: "SSD1309 / SH1106 OLED Display",
-    0x43: "ENS210   Temperature & Humidity Sensor",
-    0x4D: "MCP3x21  ADC",
-    0x50: "24Cxxx   EEPROM Memory",
-    0x51: "PCF8563  Real Time Clock",
-    0x52: "END160   TVOC Sensor",
-    0x59: "SGP40    TVOC Sensor",
-    0x5D: "GT911    Touch Panel",
-    0x60: "ATECC608 Crypto Processor / MCP472x DAC",
-    0x62: "SCD40    CO2 Sensor",
-    0x63: "ICP10111 Barometer Sensor",
-    0x68: "DS1307   Real Time Clock / MCP342x ADC",
-    0x69: "SPS30    Air Particle Sensor",
-    0x6F: "MCP7940  Real Time Clock",
-    0x76: "BME280   Temperature & Humidity Sensor",
+    0x14: "GT911",
+    0x18: "LIS331 / MCP9800",
+    0x20: "XL9535",
+    0x21: "SDP31",
+    0x32: "ZMOD4410",
+    0x38: "AHT20 / FTxxxx / HY4613",
+    0x39: "APDS9960 / VCNL4020",
+    0x3C: "SSD1309 / SH1106",
+    0x3D: "SSD1309 / SH1106",
+    0x41: "ILI2130",
+    0x43: "ENS210",
+    0x4D: "MCP3x21",
+    0x50: "24Cxxx",
+    0x51: "PCF8563",
+    0x52: "ENS160",
+    0x55: "ST1XXX",
+    0x59: "SGP40",
+    0x5D: "GT911",
+    0x60: "ATECC608 / MCP472x",
+    0x62: "SCD40",
+    0x63: "ICP10111",
+    0x68: "DS1307 / MCP342x",
+    0x69: "SPS30",
+    0x6F: "MCP7940",
+    0x76: "BME280",
 }
 
-print("Znalezione urządzenia: ", end="")
+print("Found devices: ")
 for device in devices:
-    print(f"- {device:02X} {library.get(device, "unknown")}")
+    print(f" - {device:02X}: {library.get(device, "unknown")}")
 
