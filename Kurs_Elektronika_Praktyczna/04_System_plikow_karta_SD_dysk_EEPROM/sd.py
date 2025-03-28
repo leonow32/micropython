8a39b856688fbc8afdc65bc08fc78eb87d7c7d86
leonow32 <leonow32@gmail.com>
2025-03-28 20:43:30 +0100

    Sprzątanie kodu

--- Kurs_Elektronika_Praktyczna/04_System_plikow_karta_SD_dysk_EEPROM/sd.py
+++ Kurs_Elektronika_Praktyczna/04_System_plikow_karta_SD_dysk_EEPROM/sd.py
@@ -2,44 +2,15 @@
 
 import os
 import machine
-import mem_used
+       
+#sd = machine.SDCard(slot=3, width=1, cs=13, miso=2, mosi=15, sck=14, freq=20000000)   # Płytka TTGO-T8 v1.8
+sd = machine.SDCard(slot=2, width=1, cs=5, miso=13, mosi=11, sck=12, freq=20000000)
+vfs = os.VfsFat(sd)
+os.mount(vfs, "/sd")    # znak / nie ma żadnego znaczenia i może być dowolny inny
 
-def init():
-    global sd
-    #sd = machine.SDCard(slot=3, width=1, cs=13, miso=2, mosi=15, sck=14, freq=20000000)   # Płytka TTGO-T8 v1.8
-    sd = machine.SDCard(slot=1, width=1, cs=7, miso=4, mosi=6, sck=5, freq=20000000)
-    print(sd)
-    vfs = os.VfsFat(sd)
-    os.mount(vfs, "/sd")    # znak / nie ma żadnego znaczenia i może być dowolny inny
+print(os.listdir(""))
+print(os.listdir("sd"))
 
-def check_if_folder(path):
-    atrib = os.stat(path)[0]
-    if atrib & 0x4000:
-        print(f"check_if_folder({path}) = True")
-        return True
-    else:
-        print(f"check_if_folder({path}) = False")
-        return False
-    
-def print_tree(path):
-    print(f"print_tree({path})")
-        
-    items = os.listdir(path)
-        
-    #print(items)
-    for item in items:
-        is_folder = check_if_folder(path + item)
-        print(f"{item} {is_folder}")
-        
-        if is_folder:
-            print_tree(path + item + "/")
-        
-if __name__ == "__main__":
-    mem_used.print_ram_used()
-    init()
-    print(os.listdir(""))
-    print(os.listdir("sd"))
-    #print(os.listdir("sd/katalog1"))
-    #print(os.listdir("sd/katalog2"))
-    #print_tree("/")
-    mem_used.print_ram_used()
+#print(os.listdir("sd/katalog1"))
+#print(os.listdir("sd/katalog2"))
+#print_tree("/")
