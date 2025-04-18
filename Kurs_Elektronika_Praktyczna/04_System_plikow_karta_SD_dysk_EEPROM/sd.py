8f54554fd393953dcc4e66668e3e30ac2e9916b9
leonow32 <leonow32@gmail.com>
2025-04-18 21:52:49 +0200

    Odcinek 4 gotowy

--- Kurs_Elektronika_Praktyczna/04_System_plikow_karta_SD_dysk_EEPROM/sd.py
+++ Kurs_Elektronika_Praktyczna/04_System_plikow_karta_SD_dysk_EEPROM/sd.py
@@ -3,14 +3,10 @@
 import os
 import machine
        
-#sd = machine.SDCard(slot=3, width=1, cs=13, miso=2, mosi=15, sck=14, freq=20000000)   # Płytka TTGO-T8 v1.8
-sd = machine.SDCard(slot=2, width=1, cs=5, miso=13, mosi=11, sck=12, freq=20000000)
+sd = machine.SDCard(slot=3, width=1, cs=13, miso=2, mosi=15, sck=14, freq=20000000)   # Płytka TTGO-T8 v1.8
+#sd = machine.SDCard(slot=2, width=1, cs=5, miso=13, mosi=11, sck=12, freq=20000000)
 vfs = os.VfsFat(sd)
-os.mount(vfs, "/sd")    # znak / nie ma żadnego znaczenia i może być dowolny inny
+os.mount(vfs, ".sd")    # znak / nie ma żadnego znaczenia i może być dowolny inny
 
 print(os.listdir(""))
 print(os.listdir("sd"))
-
-#print(os.listdir("sd/katalog1"))
-#print(os.listdir("sd/katalog2"))
-#print_tree("/")
