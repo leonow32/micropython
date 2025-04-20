d58c79571bf17ebf512e9cec2acd6a3013455456
leonow32 <leonow32@gmail.com>
2025-04-20 22:08:16 +0200

    Odcinek 4 gotowy

--- Kurs_Elektronika_Praktyczna/04_System_plikow_karta_SD_dysk_EEPROM/sd.py
+++ Kurs_Elektronika_Praktyczna/04_System_plikow_karta_SD_dysk_EEPROM/sd.py
@@ -3,8 +3,8 @@
 import os
 import machine
        
-sd = machine.SDCard(slot=3, width=1, cs=13, miso=2, mosi=15, sck=14, freq=20000000)   # Płytka TTGO-T8 v1.8
-#sd = machine.SDCard(slot=2, width=1, cs=5, miso=13, mosi=11, sck=12, freq=20000000)
+# sd = machine.SDCard(slot=3, width=1, cs=13, miso=2, mosi=15, sck=14, freq=20000000)   # Płytka TTGO-T8 v1.8
+sd = machine.SDCard(slot=2, width=1, cs=5, miso=13, mosi=11, sck=12, freq=20000000)
 vfs = os.VfsFat(sd)
 os.mount(vfs, ".sd")    # znak / nie ma żadnego znaczenia i może być dowolny inny
 
