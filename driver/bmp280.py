59dfe26494acb316d46a46c166e403a4ff364969
leonow32 <leonow32@gmail.com>
2025-04-25 14:28:12 +0200

    Added BMP280

--- driver/bmp280.py
+++ driver/bmp280.py
@@ -1,10 +1,12 @@
 # MicroPython 1.24.1 ESP32-S3 Octal SPIRAM
-# v1.0.0 2025.04.04
+# v1.0.0 2025.04.25
 
 import time
 
-I2C_ADDRESS = const(0x38)
-TIMEOUT     = const(10)
+# TIMEOUT     = const(10)
+
+# write nie inkrementuje adresu automatycznie
+
 
 class BMP280():
     """
@@ -12,11 +14,12 @@ class BMP280():
     - i2c: instance of I2C object.
     """
     
-    def __init__(self, i2c):
+    def __init__(self, i2c, device_address):
         time.sleep_ms(40)
         self.i2c = i2c
-        self.i2c.writeto(I2C_ADDRESS, b"\xBE\x08\x00")
-        time.sleep_ms(10)
+        self.device_address = device_address
+#         self.i2c.writeto(device_address, b"\xBE\x08\x00")
+#         time.sleep_ms(10)
         
     def __str__(self):
         return f"BMP280({str(self.i2c)})"
@@ -27,14 +30,13 @@ class BMP280():
         - Bit 7: measurement in progress.
         - Bit 3: sensor calibrated.
         """
-        buffer = self.i2c.readfrom(I2C_ADDRESS, 1)
-        return buffer[0]
+        pass
     
     def measure(self):
         """
         Start measurement process. After start you need to wait as long as bit 8 of ststur register is high.
         """
-        self.i2c.writeto(I2C_ADDRESS, b"\xAC\x33\x00")
+        pass
     
     def wait_for_ready(self):
         """
@@ -43,67 +45,22 @@ class BMP280():
         by TIMEOUT.
         """
         
-        timeout = TIMEOUT
-        while timeout:
-            buffer = self.i2c.readfrom(I2C_ADDRESS, 1)
-            if buffer[0] & 0b10000000:
-                timeout -= 1;
-                time.sleep_ms(10)
-                continue
-            else:
-                return
-        
-        raise OSError(errno.ETIMEDOUT, "Measurement time expired")
+        pass
     
     def read(self):
         """
         Reas the result of the measurement. Result is given as a dictionary with "temp" and "humi" keys.
         """
-        self.wait_for_ready()
-        buffer = self.i2c.readfrom(I2C_ADDRESS, 7)
+        pass
         
-        # Check CRC
-        # Initial value of CRC is 0xFF.
-        # CRC polynomial is X^8 + X^5 + X^4 + 1 => 0b00110001 (0x31)
-        crc = 0xFF
-        for byte in buffer[:-1]:
-            crc ^= byte
-            for i in range(8):
-                if crc & 0x80:
-                    crc <<= 1
-                    crc &= 0xFF
-                    crc ^= 0x31
-                else:
-                    crc <<= 1
-                    crc &= 0xFF
-        
-        if crc != buffer[-1]:
-            raise Exception(0, "CRC value incorrect")
-        
-        # Calculate humidity
-        humidity    = (buffer[1] << 12) | (buffer[2] << 4) | ((buffer[3] & 0xF0) >> 4)
-        humidity    = (humidity * 100) / (2**20)
-        
-        # Calculate temperature
-        temperature = ((buffer[3] & 0x0F) << 16) | (buffer[4] << 8) | (buffer[5] << 0)
-        temperature = (temperature * 200) / (2**20) - 50
-        
-        return {"temp": temperature, "humi": humidity}
 
     def reset(self):
-        self.i2c.writeto(I2C_ADDRESS, b"\xBA")
+        pass
     
     def dump(self):
         """
         Read and print all the information.
         """
-        status = self.status_get()
-        print(f"Busy:        {(status & 0b10000000) >> 7}")
-        print(f"Calibrated:  {(status & 0b00001000) >> 3}")
-        self.measure()
-        result = self.read()
-        print(f"Temperature: {result["temp"]} 'C")
-        print(f"Humidity:    {result["humi"]} %")
         pass
     
 if __name__ == "__main__":
