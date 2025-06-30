a395ea0e9a904d291af37436495c03a6d1778f6c
leonow32 <leonow32@gmail.com>
2025-06-30 21:51:19 +0200

    CRC8 works

--- utils/crc.py
+++ utils/crc.py
@@ -1,3 +1,31 @@
+def crc8(data, poly, init, xor_out, reflect_in, reflect_out):
+    crc = init;
+
+    for byte in data:
+        mask = 0b00000001 if reflect_in else 0b10000000
+        while mask:
+            bit = 1 if byte & mask else 0
+            msb = crc >> 7
+            crc = (crc << 1) & 0xFF
+
+            if bit != msb:
+                crc = crc ^ poly
+                
+            if reflect_in:
+                mask = (mask << 1) & 0xFF
+            else:
+                mask = mask >> 1
+
+    result = 0
+    
+    if reflect_out:
+        for i in range(8):
+            if crc & (1<<i):
+                result = result | (1<<(7-i))
+    else:
+        result = crc
+
+    return result ^ xor_out;
 
 
 def crc16(data, poly, init, xor_out, reflect_in, reflect_out):
@@ -32,9 +60,12 @@ def crc16(data, poly, init, xor_out, reflect_in, reflect_out):
 
 data   = bytes([0x11, 0x22, 0x44, 0x88])
 
+# result = crc8(data, 0x31, 0x00, 0x00, True, True)                # CRC-8/MAXIM-DOW
+result = crc8(data, 0x2F, 0xFF, 0xFF, False, False)                # CRC-8/AUTOSAR
+print(f"Result = {result:02X}")
+
 # result = crc16(data, 0x0589, 0x0000, 0x0000, False, False)     # CRC-16/DECT-X
-result = crc16(data, 0x8005, 0x0000, 0xFFFF, True, True)         # CRC-16/MAXIM-DOW
+# result = crc16(data, 0x8005, 0x0000, 0xFFFF, True, True)         # CRC-16/MAXIM-DOW
 # result = crc16(data, 0x1021, 0xC6C6, 0x0000, True, True)       # CRC-16/ISO-IEC-14443-3-A
 # result = crc16(data, 0x1DCF, 0xFFFF, 0xFFFF, False, False)     # CRC-16/PROFIBUS
-
-print(f"Result = {result:04X}")
+# print(f"Result = {result:04X}")
