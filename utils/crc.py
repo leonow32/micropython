a76f6b816d9cb03a243605a3ed073393fa3c5f16
leonow32 <leonow32@gmail.com>
2025-07-02 12:53:32 +0200

    Universal function for any length crc and tests

--- utils/crc.py
+++ utils/crc.py
@@ -1,3 +1,32 @@
+def crc(data, bits, poly, init, xor_out, reflect_in, reflect_out):
+    crc = init;
+
+    for byte in data:
+        mask = 0b00000001 if reflect_in else 0b10000000
+        while mask:
+            bit = 1 if byte & mask else 0
+            msb = crc >> (bits-1)
+            crc = (crc << 1) & ((2**bits)-1)
+
+            if bit != msb:
+                crc = crc ^ poly
+                
+            if reflect_in:
+                mask = (mask << 1) & 0xFF
+            else:
+                mask = mask >> 1
+
+    result = 0
+    
+    if reflect_out:
+        for i in range(bits):
+            if crc & (1<<i):
+                result = result | (1<<(bits-1-i))
+    else:
+        result = crc
+
+    return result ^ xor_out;
+
 def crc8(data, poly, init, xor_out, reflect_in, reflect_out):
     crc = init;
 
@@ -92,15 +121,11 @@ if __name__ == "__main__":
     
     
     """
-    result = crc8(data, 0x31, 0x00, 0x00, True, True)                # CRC-8/MAXIM-DOW
-    result = crc8(data, 0x2F, 0xFF, 0xFF, False, False)                # CRC-8/AUTOSAR
-    result = crc8(data, 0x1D, 0xC7, 0x00, False, False)                # CRC-8/MIFARE-MAD
-    print(f"Result = {result:02X}")
 
-    result = crc16(data, 0x0589, 0x0000, 0x0000, False, False)     # CRC-16/DECT-X
-    result = crc16(data, 0x8005, 0x0000, 0xFFFF, True, True)         # CRC-16/MAXIM-DOW
-    result = crc16(data, 0x1021, 0xC6C6, 0x0000, True, True)       # CRC-16/ISO-IEC-14443-3-A
-    result = crc16(data, 0x1DCF, 0xFFFF, 0xFFFF, False, False)     # CRC-16/PROFIBUS
+    result = crc16()     # CRC-16/DECT-X
+    result = crc16()         # CRC-16/MAXIM-DOW
+    result = crc16()       # CRC-16/ISO-IEC-14443-3-A
+    result = crc16()     # CRC-16/PROFIBUS
     print(f"Result = {result:04X}")
 
     result = crc32(data, 0x814141AB, 0x00000000, 0x00000000, False, False)     # CRC-16/AIXM
