f0037979fd66c40ceaa19508e94a619533b3763d
leonow32 <leonow32@gmail.com>
2025-07-22 22:32:51 +0200

    FT6336 polling

--- Kurs_Elektronika_Praktyczna/06_Kolorowy_wyswietlacz_TFT_z_panelem_dotykowym/demo_touch_polling.py
+++ Kurs_Elektronika_Praktyczna/06_Kolorowy_wyswietlacz_TFT_z_panelem_dotykowym/demo_touch_polling.py
@@ -4,21 +4,37 @@ from machine import Pin, SPI, I2C
 import ft6336_polling as ft6336
 import st7796_vertical as st7796
 import mem_used
+import random
 
-def draw_point(result_tuple):
-    x, y, pressed = result_tuple
-    print(f"{x:3d} {y:3d} {pressed}")
-    if(pressed):
-        display.pixel(x, y, st7796.YELLOW)
-    else:
+event_str = {
+    ft6336.EVENT_PRESS:   "Press",
+    ft6336.EVENT_LIFT:    "Lift",
+    ft6336.EVENT_CONTACT: "Contact",
+    ft6336.EVENT_NONE:    "None",
+}
+
+def draw_point(x, y, event):
+    display.fill(st7796.BLACK)
+    display.text(f"x={x:3d}, y={y:3d}, event={event_str[event]}", 0, 0, random.randint(0, 65535))
+    if event == ft6336.EVENT_PRESS:
+        display.pixel(x, y, st7796.GREEN)
+    elif event == ft6336.EVENT_CONTACT:
+        display.pixel(x, y, st7796.GREEN)
+    elif event == ft6336.EVENT_LIFT:
         display.pixel(x, y, st7796.RED)
     display.refresh()
+#     print(f"x={x:3d}, y={y:3d}, event={event_str[event]}")
+#     if(pressed):
+#         display.pixel(x, y, st7796.YELLOW)
+#     else:
+#         display.pixel(x, y, st7796.RED)
+#     display.refresh()
     
 spi = SPI(2, baudrate=80_000_000, polarity=0, phase=0, sck=Pin(15), mosi=Pin(7), miso=None)
 display = st7796.ST7796(spi, cs=Pin(4), dc=Pin(6), rst=Pin(5))
 
 i2c = I2C(0)
-touch = ft6336.FT6336(i2c, 10, draw_point)
+touch = ft6336.FT6336(i2c, 100, draw_point)
 
 display.fill(st7796.BLACK)
 display.refresh()
--- Kurs_Elektronika_Praktyczna/06_Kolorowy_wyswietlacz_TFT_z_panelem_dotykowym/ft6336_polling.py
+++ Kurs_Elektronika_Praktyczna/06_Kolorowy_wyswietlacz_TFT_z_panelem_dotykowym/ft6336_polling.py
@@ -3,30 +3,44 @@ import time
 
 ADDRESS = const(0x38)
 
+EVENT_PRESS   = const(0b00_000000)
+EVENT_LIFT    = const(0b01_000000)
+EVENT_CONTACT = const(0b10_000000)
+EVENT_NONE    = const(0b11_000000)
+
 class FT6336():
     
-    def __init__(self, i2c, period = 0, callback = None):
+    def __init__(self, i2c, period, callback):
         self.i2c = i2c
-        if callback:
-            _thread.start_new_thread(self.task, (period, callback))
+        _thread.start_new_thread(self.task, (period, callback))
         
-    def read(self):
-        buffer = self.i2c.readfrom_mem(ADDRESS, 0x03, 4)
-        x = ((buffer[0] & 0x0F) << 8) | buffer[1]
-        y = ((buffer[2] & 0x0F) << 8) | buffer[3]
-        pressed = (buffer[0] & 0b11000000 == 0x80)
-        return x, y, pressed
+#     def read(self):
+#         buffer = self.i2c.readfrom_mem(ADDRESS, 0x03, 4)
+#         x = ((buffer[0] & 0x0F) << 8) | buffer[1]
+#         y = ((buffer[2] & 0x0F) << 8) | buffer[3]
+#         pressed = (buffer[0] & 0b11000000 == 0x80)
+#         return x, y, pressed
             
     def task(self, period, callback):
-        time.sleep_ms(period)
-        prev = False
-        
         while True:
-            result = self.read()
-            if result[2]:
-                prev = True
-                callback(result)
-            elif prev and not result[2]:
-                prev = False
-                callback(result)
             time.sleep_ms(period)
+            
+            buffer = self.i2c.readfrom_mem(ADDRESS, 0x03, 4)
+            x = ((buffer[0] & 0x0F) << 8) | buffer[1]
+            y = ((buffer[2] & 0x0F) << 8) | buffer[3]
+            event = buffer[0] & 0b11000000
+            
+            callback(x, y, event)
+        
+        
+#         prev = False
+#         
+#         while True:
+#             result = self.read()
+#             if result[2]:
+#                 prev = True
+#                 callback(result)
+#             elif prev and not result[2]:
+#                 prev = False
+#                 callback(result)
+#             time.sleep_ms(period)
