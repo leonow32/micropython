69fa11be13d0be5ebf909f38fd4b3c0b0b6cc52b
leonow32 <leonow32@gmail.com>
2025-08-03 20:20:11 +0200

    WIP

--- tkinter/com_port/list_ports_tk.py
+++ tkinter/com_port/list_ports_tk.py
@@ -18,6 +18,7 @@ class MainWindow(tk.Tk):
         super().__init__()
         self.title("Skaner portów COM")
         
+        # Ramka Port COM
         self.port_frame = ttk.LabelFrame(self, text="Port COM")
         self.port_frame.pack(side=tk.TOP, padx=_padx, pady=_pady, fill=tk.X)
         
@@ -30,16 +31,35 @@ class MainWindow(tk.Tk):
         self.open_close_button = ttk.Button(self.port_frame, text="Otwórz", command=self.open_close_button_cb)
         self.open_close_button.pack(side=tk.LEFT, padx=_padx, pady=_pady)
         
+        # Ramka Terminal
+        self.terminal_frame = ttk.LabelFrame(self, text="Terminal")
+        self.terminal_frame.pack(side=tk.TOP, padx=_padx, pady=_pady, fill=tk.BOTH, expand=True)
+        
+        self.terminal_entry = ttk.Entry(self.terminal_frame)
+        self.terminal_entry.pack(side=tk.LEFT, padx=_padx, pady=_pady, fill=tk.X, expand=True)
+        
+        self.send_button = ttk.Button(self.terminal_frame, text="Wyślij", command=self.send_button_cb)
+        self.send_button.pack(side=tk.LEFT, padx=_padx, pady=_pady)
+        
+        self.read_button = ttk.Button(self.terminal_frame, text="Odbierz", command=self.read_button_cb)
+        self.read_button.pack(side=tk.LEFT, padx=_padx, pady=_pady)
+        
+        self.make_buttons_inactive()
+        
+        # Ramka Obraz
         self.image_frame = ttk.LabelFrame(self, text="Obraz")
         self.image_frame.pack(side=tk.TOP, padx=_padx, pady=_pady, fill=tk.BOTH, expand=True)
         
-        self.send = ttk.Button(self.image_frame, text="Send", command=self.send_button_cb)
+        self.send = ttk.Button(self.image_frame, text="Wyślij", command=self.send_button_cb)
         self.send.pack(side=tk.LEFT, padx=_padx, pady=_pady)
         
-        self.read = ttk.Button(self.image_frame, text="Read", command=self.read_button_cb)
+        self.read = ttk.Button(self.image_frame, text="Odbierz", command=self.read_button_cb)
         self.read.pack(side=tk.LEFT, padx=_padx, pady=_pady)
         
         self.scan_button_cb()
+        
+    def __del__(self):
+        print("Destruktor")
         
     def scan_button_cb(self):
         print("scan_button_cb")
@@ -53,9 +73,8 @@ class MainWindow(tk.Tk):
             self.listbox.set("")
         
     def open_close_button_cb(self):
-        print("open_close_button_cb")
-        
         if self.com_port_opened == False:
+            print("Port open")
             port  = self.listbox.get()
             
             if port == "":
@@ -65,19 +84,20 @@ class MainWindow(tk.Tk):
                 self.port = serial.Serial(port, 115200, timeout=0)
                 
                 self.open_close_button.configure(text="Zamknij")
+                self.make_buttons_active()
                 self.com_port_opened = True
-                
-#                 messagebox.showinfo("Informacja", f"Wybrałeś port {port}")
             
         else:
-            print("Nie gotowe")
+            print("Port close")
             self.port.close()
             self.open_close_button.configure(text="Otwórz")
             self.com_port_opened = False
-        
+            self.make_buttons_inactive()
         
     def send_button_cb(self):
-        self.port.write(b'print("Hello world")\r\n')
+        data = self.terminal_entry.get()
+        data = bytearray(data, "utf-8")
+        self.port.write(data)
     
     def read_button_cb(self):
         while True:
@@ -86,8 +106,27 @@ class MainWindow(tk.Tk):
                 break
             buf = buf.decode("utf-8")
             print(buf, end="")
+            
+    def make_buttons_active(self):
+#         self.send_button.state(["!disabled"])
+#         self.read_button.state(["!disabled"])
+#         self.terminal_entry.state(["!disabled"])
+        
+        self.send_button.configure(state="!disabled")
+        self.read_button.configure(state="!disabled")
+        self.terminal_entry.configure(state="!disabled")
+    
+    def make_buttons_inactive(self):
+#         self.send_button.state(["disabled"])
+#         self.read_button.state(["disabled"])
+#         self.terminal_entry.state(["disabled"])
+        
+        self.send_button.configure(state="disabled")
+        self.read_button.configure(state="disabled")
+        self.terminal_entry.configure(state="disabled")
 
 main_window = MainWindow()
 main_window.mainloop()
 
+del main_window
 print("Program closed")
