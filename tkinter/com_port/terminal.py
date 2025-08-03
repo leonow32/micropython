7da105e479e43337a37a50b78010a6a60d817112
leonow32 <leonow32@gmail.com>
2025-08-03 22:15:14 +0200

    WIP

--- tkinter/com_port/terminal.py
+++ tkinter/com_port/terminal.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+from tkinter import scrolledtext
 from PIL import Image
 from PIL import ImageTk
 import serial.tools.list_ports as list_ports
@@ -35,20 +36,34 @@ class MainWindow(tk.Tk):
         self.terminal_frame = ttk.LabelFrame(self, text="Terminal")
         self.terminal_frame.pack(side=tk.TOP, padx=_padx, pady=_pady, fill=tk.BOTH, expand=True)
         
-        self.terminal_entry = ttk.Entry(self.terminal_frame)
+        # Ramka Terminal 1
+        self.terminal_frame_row1 = ttk.Frame(self.terminal_frame)
+        self.terminal_frame_row1.pack(side=tk.TOP, padx=_padx, pady=_pady, fill=tk.BOTH, expand=True)
+        
+        self.terminal_entry = ttk.Entry(self.terminal_frame_row1)
         self.terminal_entry.pack(side=tk.LEFT, padx=_padx, pady=_pady, fill=tk.X, expand=True)
         
-        self.send_button = ttk.Button(self.terminal_frame, text="Wyślij", command=self.send_button_cb)
+        self.send_button = ttk.Button(self.terminal_frame_row1, text="Wyślij", command=self.send_button_cb)
         self.send_button.pack(side=tk.LEFT, padx=_padx, pady=_pady)
         
-        self.read_button = ttk.Button(self.terminal_frame, text="Odbierz", command=self.read_button_cb)
+        self.read_button = ttk.Button(self.terminal_frame_row1, text="Odbierz", command=self.read_button_cb)
         self.read_button.pack(side=tk.LEFT, padx=_padx, pady=_pady)
+        
+        self.clear_button = ttk.Button(self.terminal_frame_row1, text="Wyczyść", command=self.clear_button_cb)
+        self.clear_button.pack(side=tk.LEFT, padx=_padx, pady=_pady)
+        
+        # Ramka Terminal 2
+        self.terminal_frame_row2 = ttk.Frame(self.terminal_frame)
+        self.terminal_frame_row2.pack(side=tk.TOP, padx=_padx, pady=_pady, fill=tk.BOTH, expand=True)
+        
+        self.monitor = scrolledtext.ScrolledText(self.terminal_frame_row2, height=8, font=("Lucida Console", 15))
+        self.monitor.pack(side=tk.LEFT, padx=_padx, pady=_pady, fill=tk.BOTH, expand=True)
         
         self.make_buttons_inactive()
         
         # Ramka Obraz
         self.image_frame = ttk.LabelFrame(self, text="Obraz")
-        self.image_frame.pack(side=tk.TOP, padx=_padx, pady=_pady, fill=tk.BOTH, expand=True)
+        self.image_frame.pack(side=tk.TOP, padx=_padx, pady=_pady, fill=tk.X, expand=True)
         
         self.send = ttk.Button(self.image_frame, text="Wyślij", command=self.send_button_cb)
         self.send.pack(side=tk.LEFT, padx=_padx, pady=_pady)
@@ -75,13 +90,13 @@ class MainWindow(tk.Tk):
     def open_close_button_cb(self):
         if self.com_port_opened == False:
             print("Port open")
-            port  = self.listbox.get()
+            port_name  = self.listbox.get()
             
-            if port == "":
+            if port_name == "":
                 messagebox.showerror("Błąd", "Nie wybrałeś żadnego portu")
             else:
-                port = port[0:port.find(" ")]
-                self.port = serial.Serial(port, 115200, timeout=0)
+                port_name = port_name[0:port_name.find(" ")]
+                self.port = serial.Serial(port_name, 115200, timeout=0)
                 
                 self.open_close_button.configure(text="Zamknij")
                 self.make_buttons_active()
@@ -95,7 +110,7 @@ class MainWindow(tk.Tk):
             self.make_buttons_inactive()
         
     def send_button_cb(self):
-        data = self.terminal_entry.get()
+        data = self.terminal_entry.get() + "\r\n"
         data = bytearray(data, "utf-8")
         self.port.write(data)
     
@@ -105,25 +120,25 @@ class MainWindow(tk.Tk):
             if buf == b'':
                 break
             buf = buf.decode("utf-8")
+            
+            self.monitor.insert(tk.END, buf)
+            self.monitor.see(tk.END)
             print(buf, end="")
+    
+    def clear_button_cb(self):
+        self.monitor.delete("1.0", tk.END)
             
     def make_buttons_active(self):
-#         self.send_button.state(["!disabled"])
-#         self.read_button.state(["!disabled"])
-#         self.terminal_entry.state(["!disabled"])
-        
         self.send_button.configure(state="!disabled")
         self.read_button.configure(state="!disabled")
         self.terminal_entry.configure(state="!disabled")
+        self.monitor.configure(state="normal")
     
     def make_buttons_inactive(self):
-#         self.send_button.state(["disabled"])
-#         self.read_button.state(["disabled"])
-#         self.terminal_entry.state(["disabled"])
-        
         self.send_button.configure(state="disabled")
         self.read_button.configure(state="disabled")
         self.terminal_entry.configure(state="disabled")
+        self.monitor.configure(state="disabled")
 
 main_window = MainWindow()
 main_window.mainloop()
