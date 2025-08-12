e30eb954bd88490331ac775276ce8cbd35582237
leonow32 <leonow32@gmail.com>
2025-08-12 22:19:30 +0200

    Eksperymenty z grid

--- tkinter/com_port/terminal_grid.py
+++ tkinter/com_port/terminal_grid.py
@@ -21,48 +21,35 @@ class MainWindow(tk.Tk):
         
         # Ramka Port COM
         self.port_frame = ttk.LabelFrame(self, text="Port COM")
-        self.port_frame.grid(row=0, column=0, padx=_padx, pady=_pady, sticky="WE")
-#         self.port_frame.grid_columnconfigure(0, weight=1)
-#         self.port_frame.grid_columnconfigure(1, weight=2)
-#         self.port_frame.grid_columnconfigure(2, weight=1)
-#         self.port_frame.pack(side=tk.TOP, padx=_padx, pady=_pady, fill=tk.X)
+        self.port_frame.grid(row=0, column=0, padx=_padx, pady=_pady, sticky="NEWS")
         
         self.scan_button = ttk.Button(self.port_frame, text="Skanuj", command=self.scan_button_cb)
         self.scan_button.grid(row=0, column=0, padx=_padx, pady=_pady, sticky="we")
-#         self.scan_button.pack(side=tk.LEFT, padx=_padx, pady=_pady)
         
         self.combo = ttk.Combobox(self.port_frame, values=self.com_port_list)
         self.combo.grid(row=0, column=1, padx=_padx, pady=_pady, sticky="we", columnspan=2)
-#         self.combo.pack(side=tk.LEFT, padx=_padx, pady=_pady, fill=tk.X, expand=True)
         
         self.open_close_button = ttk.Button(self.port_frame, text="Otwórz", command=self.open_close_button_cb)
         self.open_close_button.grid(row=0, column=3, padx=_padx, pady=_pady, sticky="we")
-#         self.open_close_button.pack(side=tk.LEFT, padx=_padx, pady=_pady)
         
         # Ramka Terminal
         self.terminal_frame = ttk.LabelFrame(self, text="Terminal")
         self.terminal_frame.grid(row=1, column=0, padx=_padx, pady=_pady, sticky="we")
-#         self.terminal_frame.pack(side=tk.TOP, padx=_padx, pady=_pady, fill=tk.BOTH, expand=True)
         
         self.terminal_entry = ttk.Entry(self.terminal_frame)
         self.terminal_entry.grid(row=0, column=0, padx=_padx, pady=_pady, sticky="we")
-#         self.terminal_entry.pack(side=tk.LEFT, padx=_padx, pady=_pady, fill=tk.X, expand=True)
         
         self.send_button = ttk.Button(self.terminal_frame, text="Wyślij", command=self.send_button_cb)
         self.send_button.grid(row=0, column=1, padx=_padx, pady=_pady, sticky="we")
-#         self.send_button.pack(side=tk.LEFT, padx=_padx, pady=_pady)
         
         self.read_button = ttk.Button(self.terminal_frame, text="Odbierz", command=self.read_button_cb)
         self.read_button.grid(row=0, column=2, padx=_padx, pady=_pady, sticky="we")
-#         self.read_button.pack(side=tk.LEFT, padx=_padx, pady=_pady)
         
         self.clear_button = ttk.Button(self.terminal_frame, text="Wyczyść", command=self.clear_button_cb)
         self.clear_button.grid(row=0, column=3, padx=_padx, pady=_pady, sticky="we")
-#         self.clear_button.pack(side=tk.LEFT, padx=_padx, pady=_pady)
         
         self.monitor = scrolledtext.ScrolledText(self.terminal_frame, height=8, font=("Lucida Console", 15))
         self.monitor.grid(row=1, column=0, columnspan=4, padx=_padx, pady=_pady, sticky="news")
-#         self.monitor.pack(side=tk.LEFT, padx=_padx, pady=_pady, fill=tk.BOTH, expand=True)
         
         self.make_buttons_inactive()
         
@@ -70,11 +57,8 @@ class MainWindow(tk.Tk):
         self.image_frame = ttk.LabelFrame(self, text="Obraz")
         self.image_frame.grid(row=2, column=0, padx=_padx, pady=_pady, sticky="we")
         
-#         self.image_frame.pack(side=tk.TOP, padx=_padx, pady=_pady, fill=tk.X, expand=True)
-        
         self.zoom_label = ttk.Label(self.image_frame, text="Zoom:")
         self.zoom_label.grid(row=0, column=0, padx=_padx, pady=_pady)
-#         self.zoom_label.pack(side=tk.LEFT, padx=_padx, pady=_pady)
         
         self.zoom_combo = ttk.Combobox(self.image_frame, values=["1x", "2x", "3x", "4x"])
         self.zoom_combo.bind("<<ComboboxSelected>>", self.zoom_combo_cb)
@@ -85,6 +69,9 @@ class MainWindow(tk.Tk):
         
         self.save_button = ttk.Button(self.image_frame, text="Zapisz")
         self.save_button.grid(row=0, column=3, padx=_padx, pady=_pady, sticky="e")
+        
+        self.test_button = ttk.Button(self, text="Skanuj", command=self.scan_button_cb)
+        self.test_button.grid(row=3, column=0, padx=_padx, pady=_pady, sticky="e")
         
         self.scan_button_cb()
         
