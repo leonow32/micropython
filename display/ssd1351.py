4916817962d8e43f6dcb13bbeda88206e2549d58
leonow32 <leonow32@gmail.com>
2025-10-19 09:48:53 +0200

    New branch

---
+++ display/ssd1351.py
@@ -0,0 +1,109 @@
+# MicroPython 1.24.1 ESP32-S3 Octal SPIRAM
+# Works also with SSD1306 128x64
+
+from machine import Pin, SPI
+import framebuf
+import mem_used
+
+class SSD1309(framebuf.FrameBuffer):
+    
+    @micropython.native
+    def __init__(self, i2c, address=0x3C, flip_x=False, flip_y=False):
+        self.i2c     = i2c
+        self.address = address
+        self.flip_x  = flip_x
+        self.flip_y  = flip_y
+        self.width   = 128
+        self.height  = 64
+        self.array   = bytearray(self.width * self.height // 8)
+        super().__init__(self.array, self.width, self.height, framebuf.MONO_VLSB)
+        
+        config = (
+            0xAE,                     # Display off
+            0x20, 0x00,               # Set memory addressing mode to horizontal addressing mode
+            0x40,                     # Set display start line to 0
+            0xA0 if flip_x else 0xA1, # Set segment remap
+            0xA8, 0x3F,               # Set multiplex ratio to 63
+            0xC0 if flip_y else 0xC8, # Set COM scan direction
+            0xD3, 0x00,               # Set display offset to 0
+            0xDA, 0x12,               # Set COM pins hardware config to enable COM left/right remap, sequential COM pin config
+            0xD5, 0x80,               # Set clock and oscillator frequency to freq=8, clock=0
+            0xD9, 0xF1,               # Set pre-charge period to phase_2=F, phase_1=1
+            0xDB, 0x3C,               # Set VCOMH to max
+            0x81, 0xFF,               # Set contrast to 255 (max)
+            0xA4,                     # Use image in GDDRAM memory
+            0xA6,                     # Display not inverted
+            0x8D, 0x14,               # SSD1306 only - charge pump enable
+            0xAF,                     # Display on
+        )
+        
+        for cmd in config:
+            self.write_cmd(cmd)
+    
+    @micropython.viper
+    def __str__(self):
+        return f"SSD1309(i2c={self.i2c}, address=0x{self.address:02X}, flip_x={self.flip_x}, flip_y={self.flip_y})"
+    
+    @micropython.viper
+    def write_cmd(self, cmd: int):
+        self.i2c.writeto(self.address, bytes([0x80, cmd]))
+    
+    @micropython.viper
+    def enable(self):
+        self.write_cmd(0xAF)
+        
+    @micropython.viper
+    def disable(self):
+        self.write_cmd(0xAE)
+    
+    @micropython.viper
+    def contrast(self, value):
+        self.write_cmd(0x81)
+        self.write_cmd(value)
+    
+    @micropython.viper
+    def refresh(self):
+        # Set column address range from 0x00 to 0x7F, set page address range from 0x00 to 0x07
+        for cmd in (0x21, 0x00, 0x7F, 0x22, 0x00, 0x07):
+            self.write_cmd(cmd)
+        
+        self.i2c.writevto(self.address, (b"\x40", self.array))
+        
+    @micropython.native
+    def simulate(self):
+        print("Not implemented")
+
+if __name__ == "__main__":
+    from machine import Pin, SPI
+    from sh1106 import *
+#     from ssd1309 import *
+    import mem_used
+#     from image.down_32x32 import *
+#     from image.up_32x32 import *
+#     from font.extronic16_unicode import *
+#     from font.extronic16B_unicode import *
+
+    i2c = I2C(0) # use default pinout and clock frequency
+
+    display = SH1106(i2c, address=0x3D, flip_x=True,  flip_y=True, offset_x=2)
+#     display = SSD1309(i2c, address=0x3C, flip_x=False, flip_y=False)
+    
+    
+#     hal = DisplayHAL(display)
+#     print(hal)
+    
+#     hal.rect(0, 0, 128, 64, 1)
+#     hal.line(2, 2, 125, 61, 1)
+#     hal.circle(64, 32, 30, 1)
+#     hal.text('abcdefghijklm',  1,  2, 1)
+#     hal.text('nopqrstuvwxyz',  1, 10, 1)
+#     hal.text("abcdefghijkl",  50, 20, 1,  extronic16_unicode, "center")
+#     hal.text("abcdefghijkl",  50, 40, 0, extronic16B_unicode, "center")
+#     hal.image(up_32x32,       96,  0, 0)
+#     hal.image(down_32x32,     96, 32, 0)
+   
+    
+    hal.refresh()
+#     hal.simulate()
+
+    mem_used.print_ram_used()
