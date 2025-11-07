8dceaf21a7e5a6b843fbd6d76a7059f4ed689726
leonow32 <leonow32@gmail.com>
2025-11-07 22:15:30 +0100

    Task do rejestrowania temperatury procesora przez MQTT

--- Kurs_Elektronika_Praktyczna/10_Chmura_i_MQTT/mqtt_demo.py
+++ Kurs_Elektronika_Praktyczna/10_Chmura_i_MQTT/mqtt_demo.py
@@ -1,9 +1,11 @@
 # MicroPython 1.24.1 ESP32-S3 Octal SPIRAM
 
 import _thread
+import esp32
 import network
 import time
 import wifi_config
+import sys
 from umqtt.robust import MQTTClient
 
 def wifi_connect():
@@ -22,21 +24,27 @@ def wifi_connect():
 def mqtt_callback(topic, payload):  
     print(f"{topic} > {payload}")
 
-def listener_task(delay_ms):
+def mqtt_listener_task(delay_ms):
     while True:  
         time.sleep_ms(delay_ms)
         client.check_msg()
 
-def send_demo():
-    client.publish("0000test", "To jest test wysylania wiadomosci z ESP32")
+def mqtt_publish(topic, payload):
+    client.publish(f"0000000_Elektronika_Praktyczna/{topic}", payload)
+    
+def temperature_task(delay_ms):
+    while True:
+        client.publish("0000000_Elektronika_Praktyczna/sensor", str(esp32.mcu_temperature()))
+        time.sleep_ms(delay_ms)
  
 wifi_connect()
 client = MQTTClient(client_id="My-ESP32-S3", server="test.mosquitto.org")  
 client.set_callback(mqtt_callback)  
 client.connect()  
-client.subscribe("0000test")
-client.subscribe("0000test/qwerty")
+client.subscribe("0000000_Elektronika_Praktyczna")
+client.subscribe("0000000_Elektronika_Praktyczna/test")
+client.subscribe("0000000_Elektronika_Praktyczna/#")
 
 print("Oczekiwanie na wiadomości z MQTT")
-_thread.start_new_thread(listener_task, [1000])
-    
+_thread.start_new_thread(mqtt_listener_task, [1000])
+_thread.start_new_thread(temperature_task, [5000])
