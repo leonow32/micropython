87bf5f8fe1d0d02255da33b5d4622bb55a65c3ff
leonow32 <leonow32@gmail.com>
2025-12-26 15:26:04 +0100

    Base16

--- utils/base16.py
+++ utils/base16.py
@@ -13,18 +13,20 @@ def bytes_to_base16(data, separator=" "):
         
     return result.rstrip(separator)
 
-if __name__ == "__main__":
-    import time
-    # decoded = b"\x00"
-#     decoded = b"\xFF"
-#     decoded = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"
-#     decoded = bytearray([0xAA, 0xBB, 0xCC, 0xDD])
-    decoded = "ABCD"
-            
-    time_start = time.time()
-    encoded = bytes_to_base16(decoded, ":")
-    time_end = time.time()
-#     encoded = str_to_base16(decoded, ":")
-
-    print(f"{decoded} -> '{encoded}'")
-    print(f"time: {(time_end-time_start) * 1_000_000} us")
+def base16_to_bytes(data):
+    even = True
+    result = bytearray()
+    
+    for byte in data:
+        num = lookup.find(byte)
+        if(num == -1):
+            continue
+        
+        if even:
+            result.append(num << 4)
+        else:
+            result[-1] |= num
+        
+        even = not even
+        
+    return bytes(result)
