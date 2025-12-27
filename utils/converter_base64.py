e2dd0d9413a18495f862e9ad794755e00e38a228
leonow32 <leonow32@gmail.com>
2025-12-27 21:01:34 +0100

    Base64 converter code cleaning

--- utils/converter_base64.py
+++ utils/converter_base64.py
@@ -3,66 +3,58 @@ lookup = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 def bytes_to_base64(data):
     if isinstance(data, str):
         data = data.encode()
-        
-    counter = 0
-    length  = len(data)-1
+
     result  = ""
     
-    for byte in data:
-#         print(f"=== byte 0x{byte:02X} ===")
+    for i in range(len(data)):
         
-        # Przetwarzanie pierwszego bajtu
-        if counter % 3 == 0:
-            buffer0 = (byte & 0b11111100) >> 2
-            buffer1 = (byte & 0b00000011) << 4
-            
+        if i % 3 == 0:
+            buffer0 = (data[i] & 0b11111100) >> 2
+            buffer1 = (data[i] & 0b00000011) << 4
             result += lookup[buffer0]
             
-            # Jeżeli to był ostatni bajt w buforze wejściowym to wstawiamy wypełniacze
-            if counter == length:
-                result += lookup[buffer1]
-                result += "=="
-                print(f"return at counter = {counter}")
-                return result
+            if i == len(data)-1:
+                result += lookup[buffer1] + "=="
             
-        # Przetwarzanie drugiego bajtu
-        elif counter % 3 == 1:
-            buffer1 |= (byte & 0b11110000) >> 4
-            buffer2  = (byte & 0b00001111) << 2
-            
+        elif i % 3 == 1:
+            buffer1 |= (data[i] & 0b11110000) >> 4
+            buffer2  = (data[i] & 0b00001111) << 2
             result += lookup[buffer1]
             
-            # Jeżeli to był ostatni bajt w buforze wejściowym
-            if counter == length:
-                result += lookup[buffer2]
-                result += "="
-                print(f"return at counter = {counter}")
-                return result
+            if i == len(data)-1:
+                result += lookup[buffer2] + "="
         
-        # Przetwarzanie trzeciego bajtu
         else:
-            buffer2 |= (byte & 0b11000000) >> 6
-            buffer3  = (byte & 0b00111111) << 0
-            
+            buffer2 |= (data[i] & 0b11000000) >> 6
+            buffer3  = (data[i] & 0b00111111) << 0
             result += lookup[buffer2]
             result += lookup[buffer3]
-            
-            if counter == length:
-                print(f"return at counter = {counter}")
-                return result
-        
-        counter += 1
     
-    return ""
+    return result
+
+def base64_to_bytearray(data):
+    result = bytearray()
+    
+    for i in range(0, len(data), 4):
+        chr0 = lookup.find(str(data[i:i+1]))
+        chr1 = lookup.find(str(data[i+1:i+2]))
+        chr2 = lookup.find(str(data[i+2:i+3]))
+        chr3 = lookup.find(str(data[i+3:i+4]))
+        result.append(((chr0 << 2) & 0xFF) | (chr1 >> 4))
+        result.append(((chr1 << 4) & 0xFF) | (chr2 >> 2))
+        result.append(((chr2 << 6) & 0xFF) | (chr3 >> 2))
+        
+    return result
 
 if __name__ == "__main__":
     import measure_time
     import mem_used
-    decoded = b"\x00"
+#     decoded = b"\x00"
 #     data_in = b"\xFF"
 #     data_in = b"\x00\x00"
 #     data_in = b"\x00\x00\x00"
 #     data_in = b"\x00\x00\xFF"
+#     data_in = b"\xFF\x00\x00"
 #     data_in = b"\x00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xAA\xBB\xCC\xDD\xEE\xFF"
 #     data_in = "\x00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xAA\xBB\xCC\xDD\xEE\xFF"
     data_in  = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -70,6 +62,12 @@ if __name__ == "__main__":
     measure_time.begin()
     data_out = bytes_to_base64(data_in)
     measure_time.end("")
+
+#     data_in = "AAAA"
+    
+#     measure_time.begin()
+#     data_out = base64_to_bytearray(data_in)
+#     measure_time.end("")    
 
     print(f"{data_in} -> {data_out}")
     mem_used.print_ram_used()
