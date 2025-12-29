51b9c610e8632f920973a864f100165dbf1fcf08
leonow32 <leonow32@gmail.com>
2025-12-29 22:02:27 +0100

    Cleaning

--- utils/test_b64.py
+++ utils/test_b64.py
@@ -1,59 +1,59 @@
-from converter_base64 import *
+import b64
 
 def test_encode_1():
     data_in  = b"\x00"
     data_out = "AA=="
-    assert encode(data_in) == data_out
+    assert b64.encode(data_in) == data_out
     
 def test_encode_2():
     data_in  = b"\x00\x00"
     data_out = "AAA="
-    assert encode(data_in) == data_out
+    assert b64.encode(data_in) == data_out
 
 def test_encode_3():
     data_in  = b"\x00\x00\x00"
     data_out = "AAAA"
-    assert encode(data_in) == data_out
+    assert b64.encode(data_in) == data_out
     
 def test_encode_4():
     data_in  = b"\x00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xAA\xBB\xCC\xDD\xEE\xFF"
     data_out = "ABEiM0RVZneImaq7zN3u/w=="
-    assert encode(data_in) == data_out
+    assert b64.encode(data_in) == data_out
     
 def test_encode_5():
     data_in  = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     data_out = "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdCwgc2VkIGRvIGVpdXNtb2QgdGVtcG9yIGluY2lkaWR1bnQgdXQgbGFib3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdWEuIFV0IGVuaW0gYWQgbWluaW0gdmVuaWFtLCBxdWlzIG5vc3RydWQgZXhlcmNpdGF0aW9uIHVsbGFtY28gbGFib3JpcyBuaXNpIHV0IGFsaXF1aXAgZXggZWEgY29tbW9kbyBjb25zZXF1YXQuIER1aXMgYXV0ZSBpcnVyZSBkb2xvciBpbiByZXByZWhlbmRlcml0IGluIHZvbHVwdGF0ZSB2ZWxpdCBlc3NlIGNpbGx1bSBkb2xvcmUgZXUgZnVnaWF0IG51bGxhIHBhcmlhdHVyLiBFeGNlcHRldXIgc2ludCBvY2NhZWNhdCBjdXBpZGF0YXQgbm9uIHByb2lkZW50LCBzdW50IGluIGN1bHBhIHF1aSBvZmZpY2lhIGRlc2VydW50IG1vbGxpdCBhbmltIGlkIGVzdCBsYWJvcnVtLg=="
-    assert encode(data_in) == data_out
+    assert b64.encode(data_in) == data_out
     
 def test_decode_1():
     data_in  = "AA=="
     data_out = b'\x00'
-    assert decode(data_in) == data_out
+    assert b64.decode(data_in) == data_out
     
 def test_decode_2():
     data_in  = "AAA="
     data_out = b'\x00\x00'
-    assert decode(data_in) == data_out
+    assert b64.decode(data_in) == data_out
     
 def test_decode_3():
     data_in  = "AAAA"
     data_out = b'\x00\x00\x00'
-    assert decode(data_in) == data_out
+    assert b64.decode(data_in) == data_out
     
 def test_decode_4():
     data_in  = "AQIDBAUG"
     data_out = b'\x01\x02\x03\x04\x05\x06'
-    assert decode(data_in) == data_out
+    assert b64.decode(data_in) == data_out
     
 def test_decode_5():
     data_in  = "ABEiM0RVZneImaq7zN3u/w=="
     data_out = b'\x00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xAA\xBB\xCC\xDD\xEE\xFF'
-    assert decode(data_in) == data_out
+    assert b64.decode(data_in) == data_out
 
 def test_decode_6():
     data_in  = "AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0+P0BBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWltcXV5fYGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6e3x9fn+AgYKDhIWGh4iJiouMjY6PkJGSk5SVlpeYmZqbnJ2en6ChoqOkpaanqKmqq6ytrq+wsbKztLW2t7i5uru8vb6/wMHCw8TFxsfIycrLzM3Oz9DR0tPU1dbX2Nna29zd3t/g4eLj5OXm5+jp6uvs7e7v8PHy8/T19vf4+fr7/P3+/w=="
     data_out = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff'
-    assert decode(data_in) == data_out
+    assert b64.decode(data_in) == data_out
     
 if __name__ == "__main__":
     import pytest
