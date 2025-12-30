200fdf799330b025b85e442cbb0616bfec1b505f
leonow32 <leonow32@gmail.com>
2025-12-30 22:08:37 +0100

    WIP

--- Kurs_Elektronika_Praktyczna/12_Bluetooth_Low_Energy/ble.py
+++ Kurs_Elektronika_Praktyczna/12_Bluetooth_Low_Energy/ble.py
@@ -4,48 +4,31 @@ import bluetooth
 import binascii
 
 DEVICE_NAME = "ESP32-S3"
-conn_handle = None
-tx_handle = None
-
-# Simple
-# SIMPLE_UUID = bluetooth.UUID(1)
-# SIMPLE_CHAR = (bluetooth.UUID(2), bluetooth.FLAG_WRITE | bluetooth.FLAG_READ | bluetooth.FLAG_NOTIFY,)
-# SIMPLE_SERVICE = (SIMPLE_UUID, (SIMPLE_CHAR,),)
-
-# Heart rate
-# HR_UUID = bluetooth.UUID(0x180D)
-# HR_CHAR = (bluetooth.UUID(0x2A37), bluetooth.FLAG_WRITE | bluetooth.FLAG_READ | bluetooth.FLAG_NOTIFY,)
-# HR_SERVICE = (HR_UUID, (HR_CHAR,),)
-
-# Uart Nordic
-# UART_UUID =  bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
-# UART_RX   = (bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"), bluetooth.FLAG_WRITE,)
-# UART_TX   = (bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"), bluetooth.FLAG_READ | bluetooth.FLAG_NOTIFY,)
-# UART_SERVICE = (UART_UUID, (UART_TX, UART_RX,),)
 
 # Uart Microchip
-MUART_UUID =  bluetooth.UUID("49535343-FE7D-4AE5-8FA9-9FAFD205E455")
-MUART_RX   = (bluetooth.UUID("49535343-8841-43F4-A8D4-ECBE34729BB3"), bluetooth.FLAG_WRITE,)
-MUART_TX   = (bluetooth.UUID("49535343-1E4D-4BD9-BA61-23C647249616"), bluetooth.FLAG_READ | bluetooth.FLAG_NOTIFY,)
-MUART_SERVICE = (MUART_UUID, (MUART_TX, MUART_RX,),)
+UART_UUID =  bluetooth.UUID("49535343-FE7D-4AE5-8FA9-9FAFD205E455")
 
-# Uart test
-# TUART_UUID =  bluetooth.UUID("00000001-0000-0000-0000-000000000000")
-# TUART_RX   = (bluetooth.UUID("00000002-0000-0000-0000-000000000000"), bluetooth.FLAG_WRITE,)
-# TUART_TX   = (bluetooth.UUID("00000003-0000-0000-0000-000000000000"), bluetooth.FLAG_READ | bluetooth.FLAG_NOTIFY,)
-# TUART_SERVICE = (TUART_UUID, (TUART_TX, TUART_RX,),)
+UART_RX   = (
+    bluetooth.UUID("49535343-8841-43F4-A8D4-ECBE34729BB3"),
+    bluetooth.FLAG_WRITE,
+)
+
+UART_TX   = (
+    bluetooth.UUID("49535343-1E4D-4BD9-BA61-23C647249616"),
+    bluetooth.FLAG_READ | bluetooth.FLAG_NOTIFY,
+)
+
+UART_SERVICE = (
+    UART_UUID,
+    (UART_TX, UART_RX,),
+)
 
 # Services
-#SERVICES = (SIMPLE_SERVICE,)
-#SERVICES = (HR_SERVICE, UART_SERVICE, MUART_SERVICE)
-#SERVICES = (HR_SERVICE, UART_SERVICE,)
-#SERVICES = (HR_SERVICE,)
-# SERVICES = (UART_SERVICE,)
-SERVICES = (MUART_SERVICE,)
-#SERVICES = (TUART_SERVICE,)
+SERVICES = (UART_SERVICE,)
 
+conn_handle = None
+tx_handle   = None
 handles = ""
-ble_msg = ""
 
 events = {
     1: "Connect",
@@ -73,10 +56,8 @@ def bluetooth_interrupt(event, data):
     # _IRQ_GATTS_WRITE
     elif event == 3: #_IRQ_GATTS_WRITE:
         buffer = ble.gatts_read(data[1])
-        print(f"buffer: {buffer}")
-        global ble_msg
-        ble_msg = buffer.decode('UTF-8').strip()
-        print(f"ble_msg: {ble_msg}")
+        buffer = buffer.decode('UTF-8').strip()
+        print(f"Received: {buffer}")
         
     # _IRQ_GATTS_READ_REQUEST
     elif event == 4:
@@ -133,5 +114,4 @@ tx_handle = handles[0][0]
 advertiser(DEVICE_NAME)
 
 print(handles)
-
 
