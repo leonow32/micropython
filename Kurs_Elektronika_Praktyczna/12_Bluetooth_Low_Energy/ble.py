6abc746ead3ae8e1161fc067980df186616f5ebc
leonow32 <leonow32@gmail.com>
2026-01-02 20:40:25 +0100

    Sprzątanie

--- Kurs_Elektronika_Praktyczna/12_Bluetooth_Low_Energy/ble.py
+++ Kurs_Elektronika_Praktyczna/12_Bluetooth_Low_Energy/ble.py
@@ -2,20 +2,37 @@
 
 import bluetooth
 import binascii
+from micropython import const
 
 DEVICE_NAME = "ESP32-S3"
+
+_IRQ_CENTRAL_CONNECT    = const(1)
+_IRQ_CENTRAL_DISCONNECT = const(2)
+_IRQ_GATTS_WRITE        = const(3)
+_IRQ_GATTS_READ_REQUEST = const(4)
+_IRQ_MTU_EXCHANGED      = const(21)
+_IRQ_CONNECTION_UPDATE  = const(27)
+
+events = {
+    _IRQ_CENTRAL_CONNECT:    "Connect",
+    _IRQ_CENTRAL_DISCONNECT: "Disconnect",
+    _IRQ_GATTS_WRITE:        "Write",
+    _IRQ_GATTS_READ_REQUEST: "Read",
+    _IRQ_MTU_EXCHANGED:      "MTU Exchanged",
+    _IRQ_CONNECTION_UPDATE:  "Connection update"
+}
 
 # Uart Microchip
 UART_UUID =  bluetooth.UUID("49535343-FE7D-4AE5-8FA9-9FAFD205E455")
 
-UART_RX   = (
-    bluetooth.UUID("49535343-8841-43F4-A8D4-ECBE34729BB3"),
-    bluetooth.FLAG_WRITE,
-)
-
 UART_TX   = (
     bluetooth.UUID("49535343-1E4D-4BD9-BA61-23C647249616"),
     bluetooth.FLAG_READ | bluetooth.FLAG_NOTIFY,
+)
+
+UART_RX   = (
+    bluetooth.UUID("49535343-8841-43F4-A8D4-ECBE34729BB3"),
+    bluetooth.FLAG_WRITE,
 )
 
 UART_SERVICE = (
@@ -27,52 +44,24 @@ UART_SERVICE = (
 SERVICES = (UART_SERVICE,)
 
 conn_handle = None
-tx_handle   = None
-handles = ""
-
-events = {
-    1: "Connect",
-    2: "Disconnect",
-    3: "Write",
-    4: "Read",
-    21: "MTU Exchanged",
-    27: "Connection update"
-}
 
 def bluetooth_interrupt(event, data):
     print(f"- bluetooth_interrupt, event={events[event]}, data={data}")
     
-    # _IRQ_CENTRAL_CONNECT
-    if event == 1:
+    if event == _IRQ_CENTRAL_CONNECT:
         global conn_handle
         conn_handle = data[0]
 
-    # _IRQ_CENTRAL_DISCONNECT
-    elif event == 2:
+    elif event == _IRQ_CENTRAL_DISCONNECT:
         global conn_handle
         conn_handle = None
         advertiser(DEVICE_NAME)
     
-    # _IRQ_GATTS_WRITE
-    elif event == 3: #_IRQ_GATTS_WRITE:
+    elif event == _IRQ_GATTS_WRITE:
         buffer = ble.gatts_read(data[1])
-        buffer = buffer.decode('UTF-8').strip()
+        buffer = buffer.decode('UTF-8')
+        buffer = buffer.strip()
         print(f"Received: {buffer}")
-        
-    # _IRQ_GATTS_READ_REQUEST
-    elif event == 4:
-        pass
-        
-    # _IRQ_MTU_EXCHANGED
-    elif event == 21:
-        pass
-    
-    # _IRQ_CONNECTION_UPDATE
-    elif event == 27:
-        pass
-    
-    else:
-        print("other")
 
 def advertiser(device_name):
     print(f"advertiser({device_name})")
@@ -80,7 +69,6 @@ def advertiser(device_name):
         device_name = bytes(device_name, "utf-8")
     advertisement_data = bytearray(b'\x02\x01\x02') + bytearray((len(device_name) + 1, 0x09)) + device_name
     ble.gap_advertise(100, advertisement_data)
-    print(advertisement_data)
 
 def get_my_mac():
     mac = ble.config("mac")
@@ -92,26 +80,22 @@ def get_my_mac():
 
 def ble_send(text):
     if conn_handle is None:
-        print("Brak połączenia BLE")
+        print("No connection")
         return
 
     if isinstance(text, str):
         text = text.encode("utf-8")
 
     ble.gatts_notify(conn_handle, tx_handle, text)
-    print(f"Wysłano BLE: {text}")
+    print(f"Sent: {text}")
     
 ble = bluetooth.BLE()
 ble.active(True)
 ble.irq(bluetooth_interrupt)
-print(f"Mój adres MAC: {get_my_mac()}")
+print(f"MAC Address: {get_my_mac()}")
 
-# register_services()
 handles = ble.gatts_register_services(SERVICES)
-# handles = ((tx_handle, rx_handle),)
 tx_handle = handles[0][0]
+rx_handle = handles[0][1]
 
 advertiser(DEVICE_NAME)
-
-print(handles)
-
