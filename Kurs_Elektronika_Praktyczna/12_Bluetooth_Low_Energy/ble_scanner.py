92f415640c4d15bd2c31f0c0e294e012f5a546b3
leonow32 <leonow32@gmail.com>
2026-01-08 22:28:46 +0100

    Odcinek 12 gotowy

--- Kurs_Elektronika_Praktyczna/12_Bluetooth_Low_Energy/ble_scanner.py
+++ Kurs_Elektronika_Praktyczna/12_Bluetooth_Low_Energy/ble_scanner.py
@@ -1,15 +1,16 @@
 import bluetooth
 import ubinascii
 import time
-import micropython
 from micropython import const
 
-SCAN_TIME_S = const(10)
+# Czas skanowania [s]
+SCAN_TIME_S = const(100)
 
 _IRQ_SCAN_RESULT = const(5)
 _IRQ_SCAN_DONE   = const(6)
 
-# Słownik na urządzenia: {adres_mac: [lp, rssi, nazwa]}
+# Słownik do zapisywania znalezionych urządzeń
+# {adres_mac: [rssi, name]}
 devices = {}
 
 def decode_name(payload):
@@ -17,67 +18,40 @@ def decode_name(payload):
     while i+1 < len(payload):
         length = payload[i]
         type = payload[i+1]
-#         if type in (0x08, 0x09):
         if type == 0x08 or type == 0x09:
             try:
                 return str(payload[i + 2 : i + length + 1], "utf-8")
             except:
                 return "Błąd dekodowania"
         i += length + 1
-    return None # Zwracamy None zamiast "Nieznane", by łatwiej było zarządzać logiką
+    return None
 
-# def print_line(data):
-#     mac, rssi, name = data
-#     print(f"{mac} | {rssi:4} dBm | {name}")
-
-def ble_irq(event, data):    
+def bluetooth_interrupt(event, data):    
     if event == _IRQ_SCAN_RESULT:
         addr_type, mac, adv_type, rssi, adv_data = data
         mac = ubinascii.hexlify(mac, ":").decode().upper()
-        
         name = decode_name(adv_data)
         
         # Jeśli urządzenia nie ma jeszcze w słowniku
         if mac not in devices:
-#             display_name = name if name else "Nieznane"
-#             devices[mac] = [rssi, display_name]
             devices[mac] = [rssi, name]
-            
-            # Wypisz nowo znalezione urządzenie
-#             micropython.schedule(print_line, (mac, rssi, display_name))
-            print(f"{mac} | {rssi:3} dBm | {name}")
+            print(f"{mac} | {rssi:4} dBm | {name}")
         
         # Jeśli urządzenie już było, ale nie miało nazwy, a teraz ją znaleźliśmy
-#         elif name is not None and devices[mac][1] == "Nieznane":
-        elif name is not None and devices[mac][1] == None:
+        elif devices[mac][1] == None and name is not None:
             devices[mac][1] = name
-#             lp = devices[mac][0]
-            # Ponowne wypisanie tej samej linii z nową nazwą
-#             micropython.schedule(print_line, (mac, rssi, f"{name} (zaktualizowano)"))
-            print(f"{mac} | {rssi:3} dBm | {name} (nazwa z charakterystyki)")
+            print(f"{mac} | {rssi:4} dBm | {name} (nazwa z charakterystyki)")
 
     elif event == _IRQ_SCAN_DONE:
-#         micropython.schedule(lambda _: print("-" * 85 + "\nSkanowanie zakończone."), None)
         print("------------------------------------------")
         print(f"Znaleziono {len(devices)} urządzeń")
 
 ble = bluetooth.BLE()
 ble.active(True)
-
-devices.clear()
+ble.irq(bluetooth_interrupt)
+ble.gap_scan(SCAN_TIME_S * 1000, 30000, 30000, True)
 
 print("    Adres MAC     |   RSSI   | Nazwa")
 print("------------------------------------------")
 
-ble.irq(ble_irq)
-
-# KLUCZOWA ZMIANA: active=True
-# Interwał i okno ustawione na 30ms (ciągłe skanowanie)
-ble.gap_scan(SCAN_TIME_S * 1000, 30000, 30000, True)
-
 time.sleep(SCAN_TIME_S + 1)
-
-#     end_time = time.ticks_add(time.ticks_ms(), duration_ms + 1000)
-#     while time.ticks_diff(end_time, time.ticks_ms()) > 0:
-#         time.sleep(0.1)
-
