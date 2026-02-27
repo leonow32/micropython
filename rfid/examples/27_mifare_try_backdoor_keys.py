2a03ad258cec9a2796ed732dbd68cb7ec89e3754
leonow32 <leonow32@gmail.com>
2026-02-27 11:14:30 +0100

    Scan for backboor keys

---
+++ rfid/examples/27_mifare_try_backdoor_keys.py
@@ -0,0 +1,15 @@
+from machine import Pin, SPI
+from rfid.rc522 import RC522
+from rfid.iso_iec_14443_3 import ISO_IEC_14443_3
+from rfid.mifare_classic import MifareClassic
+from rfid.log import *
+
+spi = SPI(0, baudrate=10_000_000, polarity=0, phase=0, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
+cs  = Pin(5)
+rst = Pin(7)
+pcd = RC522(spi, cs, rst)
+iso = ISO_IEC_14443_3(pcd)
+mif = MifareClassic(pcd, iso)
+
+debug_disable()
+mif.backdoor_try_keys(64)
