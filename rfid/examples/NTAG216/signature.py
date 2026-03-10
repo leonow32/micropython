602594ff6e5da7f332a4e982d660a2ff5ee619c8
leonow32 <leonow32@gmail.com>
2026-03-10 23:06:47 +0100

    Signature

---
+++ rfid/examples/NTAG216/signature.py
@@ -0,0 +1,19 @@
+from machine import Pin, SPI
+from rfid.rc522 import RC522
+from rfid.iso_iec_14443_3 import ISO_IEC_14443_3
+from rfid.ntag21x import NTAG21X
+from rfid.log import *
+
+spi = SPI(0, baudrate=10_000_000, polarity=0, phase=0, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
+cs  = Pin(5)
+rst = Pin(7)
+pcd = RC522(spi, cs, rst)
+iso = ISO_IEC_14443_3(pcd)
+mif = NTAG21X(pcd)
+
+iso.scan_and_select()
+
+print("Read ECC sigature")
+data = mif.signature_read()
+
+debug("Signature", data)
