0ae40659a2de9955c2365065bf582a5a6d174829
leonow32 <leonow32@gmail.com>
2026-03-12 22:18:53 +0100

    WIP brute force

--- rfid/examples/NTAG216/brute_force.py
+++ rfid/examples/NTAG216/brute_force.py
@@ -3,10 +3,9 @@ from rfid.rc522 import RC522
 from rfid.iso_iec_14443_3 import ISO_IEC_14443_3
 from rfid.ntag21x import NTAG21X
 from rfid.log import *
-from struct import pack
 import measure_time
 
-spi = SPI(0, baudrate=10_000_000, polarity=0, phase=0, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
+spi = SPI(0, baudrate=20_000_000, polarity=0, phase=0, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
 cs  = Pin(5)
 rst = Pin(7)
 pcd = RC522(spi, cs, rst)
@@ -25,14 +24,7 @@ for i in range(0xFFFFFF00, 0xFFFFFFFF+1):
     print(f"Try {i:08X}: ", end="")
     
     try:
-        pwd[0] = (i >> 24) & 0xFF
-        pwd[1] = (i >> 16) & 0xFF
-        pwd[2] = (i >> 8) & 0xFF
-        pwd[3] = i & 0xFF
-        mif.authenticate(pwd)
-        
-#         mif.authenticate(pack(">I", i))
-        
+        mif.authenticate(i.to_bytes(4))
         print("success")
         break
     except:
