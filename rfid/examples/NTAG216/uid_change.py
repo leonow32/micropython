b315f2b43a200b59fa3ff6a9663c93708f9c1012
leonow32 <leonow32@gmail.com>
2026-03-12 21:30:02 +0100

    Demo of UID change

---
+++ rfid/examples/NTAG216/uid_change.py
@@ -0,0 +1,26 @@
+from machine import Pin, SPI
+from rfid.rc522 import RC522
+from rfid.iso_iec_14443_3 import ISO_IEC_14443_3
+from rfid.ntag21x import NTAG21X
+from rfid.log import *
+
+spi = SPI(0, baudrate=10_000_000, polarity=0, phase=0, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
+cs  = Pin(5)
+rst = Pin(7)
+pcd = RC522(spi, cs, rst)
+iso = ISO_IEC_14443_3(pcd)
+mif = NTAG21X(pcd)
+
+uid, atqa, sak = iso.scan_and_select()
+print("Before operation")
+debug("UID", uid)
+print(f"ATQA: {atqa:04X}")
+print(f"SAK:  {sak:02X}")
+
+mif.uid_change(b"\x11\x22\x33\x44\x55\x66\x77")
+
+uid, atqa, sak = iso.scan_and_select()
+print("After operation")
+debug("UID", uid)
+print(f"ATQA: {atqa:04X}")
+print(f"SAK:  {sak:02X}")
