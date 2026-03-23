bc40aa46e8ec3ee521b98c9683e3c163fc060c0f
leonow32 <leonow32@gmail.com>
2026-03-23 13:32:46 +0100

    CRC demo

--- Kurs_RFID/Odcinek_1/antenna_on.py
+++ Kurs_RFID/Odcinek_1/antenna_on.py
@@ -2,7 +2,7 @@ from machine import Pin, SPI
 from rfid.rc522 import RC522
 from rfid.log import *
 
-spi = SPI(0, baudrate=10_000_000, polarity=0, phase=0, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
+spi = SPI(0, baudrate=1_000_000, polarity=0, phase=0, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
 cs  = Pin(5)
 rst = Pin(7)
 pcd = RC522(spi, cs, rst)
