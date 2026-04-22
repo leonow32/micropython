4ed774feb88691f1259aeb2d78c09afa25a78291
leonow32 <leonow32@gmail.com>
2026-04-22 22:08:38 +0200

    All chars demo ready

--- display_hal_examples/mono_lcd_oled/font_print_all_chars.py
+++ display_hal_examples/mono_lcd_oled/font_print_all_chars.py
@@ -6,14 +6,12 @@ import mem_used
 import measure_time
 
 from display_hal.display_hal import *
-from display_hal.font.dos16 import *
-from display_hal.font.extronic16B_unicode import *
 
 # Display OLED 128x64 monochrome with SSD1309
-# from machine import I2C
-# from display_hal.driver.ssd1309 import *
-# i2c     = I2C(0) # use default pinout and clock frequency
-# display = SSD1309(i2c, address=0x3C, rotate=False)
+from machine import I2C
+from display_hal.driver.ssd1309 import *
+i2c     = I2C(0) # use default pinout and clock frequency
+display = SSD1309(i2c, address=0x3C, rotate=False)
 
 # Display OLED 128x64 monochrome with SH1106
 # from machine import I2C
@@ -22,10 +20,10 @@ from display_hal.font.extronic16B_unicode import *
 # display = SH1106(i2c, address=0x3D, rotate=False, offset_x=2)
 
 # Display OLED 128x160 monochrome with SH1108
-from machine import Pin, SPI
-from display_hal.driver.sh1108 import *
-spi = SPI(1, baudrate=10_000_000, polarity=0, phase=0)
-display = SH1108(spi, cs=Pin(4), dc=Pin(2), rotate=1, offset_x=16)
+# from machine import Pin, SPI
+# from display_hal.driver.sh1108 import *
+# spi = SPI(1, baudrate=10_000_000, polarity=0, phase=0)
+# display = SH1108(spi, cs=Pin(4), dc=Pin(2), rotate=1, offset_x=16)
 
 # Display LCD DEM128064E1 from Display Elektronik GmbH with ST7565R
 # from machine import Pin, PWM, SPI
@@ -37,67 +35,66 @@ display = SH1108(spi, cs=Pin(4), dc=Pin(2), rotate=1, offset_x=16)
 dihal   = DisplayHAL(display)
 print(dihal)
 
-def print_char(hal, font, x, y):
-    pass
-    
-        
 def print_all_characters(hal, font):
     x = 0
     y = 0
-    n = 0
-    loop = 0
-    char_height, char_space = font[-1]
     
     hal.clear()
-                
-    for index, data in font.items():
-        print(f"loop: {loop}, index: {index}, data: {data}")
+
+    for index in sorted(font):
+        data = font[index]
         
         if index == -1:
-            print("aaaaa")
+            char_height, char_space = data
             continue
             
         else:
             char_width = data[0]
             
-            # Sprawdzenie czy znak pasuje w kierunku x
-            if x + char_width + char_space < hal.width:
+            # Check if a character fits on the current line
+            if x + char_width + char_space <= hal.width:
                 hal.text(chr(index), x, y, font)
                 x += char_width + char_space
+            
+            # The character does not fit on the current line.
             else:
-                hal.circle(30, 30, 10)
-                hal.refresh()
-                input("Press enter")
                 x = 0
-                y = 0
-                hal.clear()
-            
-            
-            
-            
-            
-            
-            # Verify is this char fits into remaining area of the screen
-#             if x + char_width + char_space < hal.width and y + char_height < hal.height:
-#                 hal.text(chr(n), x, y, font)
-#                 char_printed = True
-#             else:
-#                 char_printed = False
-#             
-#             x += char_width + char_space
-#             if x >= hal.width:
-#                 x = 0
-#                 y += char_height
-#                 if
+                y += char_height
                 
-            
+                # Checking if the character height fits on a new line
+                if y + char_height <= hal.height:
+                    hal.text(chr(index), x, y, font)
+                    x += char_width + char_space
                 
-        
+                # The character does not fit on a new line -> refresh the screen
+                else:
+                    hal.refresh()
+                    input("Press enter")
+                    
+                    x = 0
+                    y = 0
+                    hal.clear()
+                    hal.text(chr(index), x, y, font)
+                    x += char_width + char_space
+    
+    hal.refresh()
     
 # Print all the characters in positive
 dihal.color_set(1, 0)
-# print_all_characters(dihal, dos16)
-print_all_characters(dihal, extronic16B_unicode)
+
+# from display_hal.font.console7 import console7 as font
+# from display_hal.font.dos8 import dos8 as font
+# from display_hal.font.dos16 import dos16 as font
+# from display_hal.font.extronic16_unicode import extronic16_unicode as font
+# from display_hal.font.extronic16B_unicode import extronic16B_unicode as font
+# from display_hal.font.galaxy16_digits import galaxy16_digits as font
+# from display_hal.font.galaxy24_digits import galaxy24_digits as font
+# from display_hal.font.micro8 import micro8 as font
+# from display_hal.font.mini8 import mini8 as font
+# from display_hal.font.mini8B import mini8B as font
+# from display_hal.font.sans24 import sans24 as font
+from display_hal.font.sans24B import sans24B as font
+
+print_all_characters(dihal, font)
 
 mem_used.print_ram_used()
-
