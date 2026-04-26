87e0220eec137f08b15d4c981e4e793a2bf2fae4
leonow32 <leonow32@gmail.com>
2026-04-26 19:47:11 +0200

    Testing

--- display_hal_examples/mono_lcd_oled/image_icons.py
+++ display_hal_examples/mono_lcd_oled/image_icons.py
@@ -18,10 +18,10 @@ from display_hal.image_mono.settings_32x32 import *
 from display_hal.image_mono.up_32x32 import *
 
 # Display OLED 128x64 monochrome with SSD1309
-# from machine import I2C
-# from display_hal.driver.ssd1309 import *
-# i2c     = I2C(0) # use default pinout and clock frequency
-# display = SSD1309(i2c, address=0x3C, rotate=False)
+from machine import I2C
+from display_hal.driver.ssd1309 import *
+i2c     = I2C(0) # use default pinout and clock frequency
+display = SSD1309(i2c, address=0x3C, rotate=False)
 
 # Display OLED 128x64 monochrome with SH1106
 # from machine import I2C
@@ -30,10 +30,10 @@ from display_hal.image_mono.up_32x32 import *
 # display = SH1106(i2c, address=0x3D, rotate=False, offset_x=2)
 
 # Display OLED 128x160 monochrome with SH1108
-# from machine import Pin, SPI
-# from display_hal.driver.sh1108 import *
-# spi = SPI(1, baudrate=10_000_000, polarity=0, phase=0)
-# display = SH1108(spi, cs=Pin(4), dc=Pin(2), rotate=1, offset_x=16)
+from machine import Pin, SPI
+from display_hal.driver.sh1108 import *
+spi = SPI(1, baudrate=10_000_000, polarity=0, phase=0)
+display = SH1108(spi, cs=Pin(4), dc=Pin(2), rotate=1, offset_x=16)
 
 # Display LCD DEM128064E1 128x64 from Display Elektronik GmbH with ST7565R
 # from machine import Pin, PWM, SPI
@@ -43,11 +43,11 @@ from display_hal.image_mono.up_32x32 import *
 # display = DEM128064E1(spi, cs=Pin(5), dc=Pin(6), rst=Pin(7))
 
 # Display LCD DEM240064B 240x64 from Display Elektronik GmbH with ST7565P
-from machine import Pin, PWM, SPI
-from display_hal.driver.dem240064b import *
-pwm = PWM(Pin(28), freq=50000, duty_u16=65535)
-spi = SPI(0, baudrate=10_000_000, polarity=0, phase=0, sck=Pin(18), mosi=Pin(19), miso=Pin(16))
-display = DEM240064B(spi, cs0=Pin(17), cs1=Pin(22), dc=Pin(20), rst=Pin(21))
+# from machine import Pin, PWM, SPI
+# from display_hal.driver.dem240064b import *
+# pwm = PWM(Pin(28), freq=50000, duty_u16=65535)
+# spi = SPI(0, baudrate=10_000_000, polarity=0, phase=0, sck=Pin(18), mosi=Pin(19), miso=Pin(16))
+# display = DEM240064B(spi, cs0=Pin(17), cs1=Pin(22), dc=Pin(20), rst=Pin(21))
 
 dihal   = DisplayHAL(display)
 print(dihal)
