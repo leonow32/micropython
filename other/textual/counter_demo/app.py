5c52bc8e83ec6d541370cfc887bfb02221884d2b
leonow32 <leonow32@gmail.com>
2026-06-11 13:55:55 +0200

    Textual demo

--- other/textual/counter_demo/app.py
+++ other/textual/counter_demo/app.py
@@ -1,23 +1,45 @@
 from textual.app import App
-from textual.widgets import Header, Footer, Button
+from textual.containers import ScrollableContainer
+from textual.widgets import Header, Footer, Button, Static
 
+class TimeDisplay(Static):
+    pass
+
+class Stopwatch(Static):
+    def compose(self):
+        yield Button("Czerwony", variant="error")
+        yield Button("Żółty", variant="warning")
+        yield Button("Zielony", variant="success")
+        yield Button("Niebieski", variant="primary")
+        yield Button("Czarny", variant="default")
+        yield Button("Reset")
+        yield TimeDisplay("00:00:00.00")
+    
 class StopwatchApp(App):
     BINDINGS = [
         ("d,ctrl+a", "toggle_dark_mode", "Toggle light/dark mode")
     ]
     
     def compose(self):
-        self.theme = "textual-light"
+        self.theme = "textual-dark"
         yield Header(show_clock=True)
-        yield Button("Start")
-        yield Button("Stop")
+        with ScrollableContainer(id="stopwatches"):
+            yield Stopwatch()
+            yield Stopwatch()
+            yield Stopwatch()
+#         yield ScrollableContainer(
+#             Stopwatch(),
+#             Stopwatch(),
+#             Stopwatch(),
+#             id="stopwatches"
+#         )
         yield Footer()
         
     def action_toggle_dark_mode(self):
-        if self.theme == "textual-dark":
-            self.theme = "textual-light"
-        else:
+        if self.theme == "textual-light":
             self.theme = "textual-dark"
+        else:
+            self.theme = "textual-light"
 
 if __name__ == "__main__":
     app = StopwatchApp()
