270792b572900cab3d34ea031ea41480c1c1e9b6
leonow32 <leonow32@gmail.com>
2026-06-14 22:12:55 +0200

    Textual learning

--- other/textual/counter_demo/buttons.py
+++ other/textual/counter_demo/buttons.py
@@ -8,47 +8,60 @@ from textual.widgets import Header, Footer, Button, Digits, Button, Static, Text
 class Application(App):
     BINDINGS = [
         Binding("escape", "quit", "Wyjście", key_display="ESC"), # Wbudowane, nie trzeba definiować quit
-        ("d,ctrl+a", "toggle_dark_mode", "Toggle light/dark mode"),
-        ("a", "add_stopwatch", "Add"),
-        ("r", "remove_stopwatch", "Remove"),
+        Binding("ctrl+q", "test", "Wyjście", show=False), # Wbudowane, nie trzeba definiować quit
+        ("t", "test", "Test"),
     ]
     
-    CSS_PATH = "containers.tcss"
+    CSS_PATH = "buttons.tcss"
     
     def compose(self):
-        yield Header(name="Demo kontenerów", show_clock=True)
+        yield Header(name="Demo kontenerów", show_clock=True, icon=None)
 
-        
-        yield Button("Czerwony", variant="error")
-        yield Button("Żółty", variant="warning")
-        yield Button("Zielony", variant="success")
-        yield Button("Niebieski", variant="primary")
-        yield Button("Czarny")
+        yield Button("Czerwony",  id="red",   variant="error")
+        yield Button("Żółty",     id="yellow",variant="warning")
+        yield Button("Zielony",   id="green", variant="success")
+        yield Button("Niebieski", id="blue",  variant="primary")
+        yield Button("Czarny",    id="black", variant="default")
 
-        # yield Right(
-           
-            # classes="box"
-        # )
-
-        # with Center():
-        #     Digits("0123456789")
-        #     Digits("ABCDEFGHIJ")
-
-        yield Vertical(
-            # Digits("3.141,592,653,5897"),
-            # TextArea(),
-            # TextArea(),
-            Static("aaa", classes="box"),
-            Static("bbb", classes="box"),
-            classes="box"
-        )
         yield Footer()
         
     def on_mount(self):
         self.title = "Demo przycisków"
 
+        header = self.query_one(Header)
+        header.icon = "Menu"
+
         footer = self.query_one(Footer)
         footer.show_command_palette = False
+
+    def action_test(self):
+        print("test")
+        self.notify("Test")
+
+    @on(Button.Pressed, "#red")
+    def button_red(self):
+        print("Wcisnąłeś czerwony przycisk")
+        self.notify("Wcisnąłeś [b]czerwony[/b] przycisk", title="Błąd", severity="error")
+
+    @on(Button.Pressed, "#yellow")
+    def button_yellow(self):
+        print("Wcisnąłeś żółty przycisk")
+        self.notify("Wcisnąłeś [i]żółty[i] przycisk", title="Ostrzeżenie", severity="warning")
+    
+    @on(Button.Pressed, "#green")
+    def button_green(self):
+        print("Wcisnąłeś zielony przycisk")
+        self.notify("Wcisnąłeś [u]zielony[/u] przycisk", title="Informacja", severity="information")
+
+    @on(Button.Pressed, "#blue")
+    def button_blue(self):
+        print("Wcisnąłeś niebieski przycisk")
+        self.notify("Wcisnąłeś niebieski przycisk", title="Informacja")
+
+    @on(Button.Pressed, "#black")
+    def button_black(self):
+        print("Wcisnąłeś czarny przycisk")
+        self.notify("Wcisnąłeś czarny przycisk")
 
 if __name__ == "__main__":
     app = Application()
