a5a85847c6f3eb92ca35343e0129a4e2c08ef8ae
leonow32 <leonow32@gmail.com>
2026-06-15 13:47:49 +0200

    Osobna klasa na pop-up z pytaniem o wyjście

---
+++ other/textual/app/buttons.py
@@ -0,0 +1,87 @@
+from textual import on
+from textual.app import App
+from textual.binding import Binding
+from textual.containers import Horizontal, Vertical, Middle, Center, ScrollableContainer, Center, Right
+from textual.reactive import Reactive
+from textual.screen import ModalScreen
+from textual.widgets import Header, Footer, Button, Digits, Button, Static, TextArea, Label
+
+from my_widgets.confirm_exit import ConfirmExit
+
+class Application(App):
+    CSS_PATH = "buttons.tcss"
+
+    BINDINGS = [
+        Binding("escape", "confirm_exit", "Wyjście", key_display="ESC"), # Wbudowane, nie trzeba definiować quit
+        Binding("ctrl+q", "test", "Wyjście", show=False), # Wbudowane, nie trzeba definiować quit
+        ("p", "pop_up", "Testowy pop-up"),
+        ("t", "test", "Test"),
+    ]
+    
+    def compose(self):
+        yield Header(name="Demo kontenerów", show_clock=True, icon=None)
+
+        yield Vertical(
+            Button("Czerwony",  id="red",   variant="error"),
+            Button("Żółty",     id="yellow",variant="warning"),
+            Button("Zielony",   id="green", variant="success"),
+            Button("Niebieski", id="blue",  variant="primary"),
+            Button("Czarny",    id="black", variant="default"),
+            classes = "backgound_hatch"
+        )
+
+        # yield Button("Czerwony",  id="red",   variant="error")
+        # yield Button("Żółty",     id="yellow",variant="warning")
+        # yield Button("Zielony",   id="green", variant="success")
+        # yield Button("Niebieski", id="blue",  variant="primary")
+        # yield Button("Czarny",    id="black", variant="default")
+
+        yield Footer()
+        
+    def on_mount(self):
+        self.title = "Demo przycisków"
+
+        header = self.query_one(Header)
+        header.icon = "Menu"
+
+        footer = self.query_one(Footer)
+        footer.show_command_palette = False
+
+    def action_test(self):
+        print("test")
+        self.notify("Test")
+        
+    def action_pop_up(self):
+        self.push_screen(ConfirmExit())  # pokazanie popupu
+
+    def action_confirm_exit(self):
+        self.push_screen(ConfirmExit())
+
+    @on(Button.Pressed, "#red")
+    def button_red(self):
+        print("Wcisnąłeś czerwony przycisk")
+        self.notify("Wcisnąłeś [b]czerwony[/b] przycisk", title="Błąd", severity="error")
+
+    @on(Button.Pressed, "#yellow")
+    def button_yellow(self):
+        print("Wcisnąłeś żółty przycisk")
+        self.notify("Wcisnąłeś [i]żółty[i] przycisk", title="Ostrzeżenie", severity="warning")
+    
+    @on(Button.Pressed, "#green")
+    def button_green(self):
+        print("Wcisnąłeś zielony przycisk")
+        self.notify("Wcisnąłeś [u]zielony[/u] przycisk", title="Informacja", severity="information")
+
+    @on(Button.Pressed, "#blue")
+    def button_blue(self):
+        print("Wcisnąłeś niebieski przycisk")
+        self.notify("Wcisnąłeś niebieski przycisk", title="Informacja")
+
+    @on(Button.Pressed, "#black")
+    def button_black(self):
+        print("Wcisnąłeś czarny przycisk")
+        self.notify("Wcisnąłeś czarny przycisk")
+
+if __name__ == "__main__":
+    app = Application()
+    app.run()
